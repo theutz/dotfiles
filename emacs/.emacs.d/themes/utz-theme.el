959a8e7bb42561283e57ea0abfef7fb42ac9dce0
Michael Utz <michael@theutz.com>
2020-03-13 12:20:34 +0200

    [emacs] dramatic theme re-do starts

---
+++ emacs/.emacs.d/themes/utz-theme.el
@@ -0,0 +1,10 @@
+(deftheme utz
+  "All the stuff I love.")
+
+(custom-theme-set-faces
+ 'utz
+ '(default ((t (:background "purple4" :foreground "white" :height 140 :family "JetBrainsMono Nerd Font"))))
+ '(cursor ((t (:background "white" :foreground "purple4"))))
+ '(region ((t (:background "yellow3" :foreground "purple4")))))
+
+(provide-theme 'utz)
