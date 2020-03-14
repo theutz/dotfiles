ef78e0f1a5b7c4389a553d3720014e531e3b7f81
Michael Utz <michael@theutz.com>
2020-03-14 20:32:30 +0200

    [emacs] The Joy of Emacs

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -2,4 +2,5 @@
  ("space" . ((user-emacs-directory . "~/.spacemacs")
                  (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
  ("doom" . ((user-emacs-directory . "~/.doom-emacs")
-            (env . (("DOOMDIR" . "~/.doom.d"))))))
+            (env . (("DOOMDIR" . "~/.doom.d")))))
+ ("joy" . ((user-emacs-directory . "~/.the-joy-of-emacs"))))
