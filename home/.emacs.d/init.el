5bb5ba73ded6c743ccaeffc3dc702523f634b654
Michael Utz <michael@theutz.com>
2020-06-27 18:56:26 +0300

    Get basic environment setup

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,20 +1,35 @@
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (setq straight-use-package-by-default t)
-  (load bootstrap-file nil 'nomessage))
+;;;; init.el -- The main starting point.
+;; =====================================
 
-(straight-use-package 'use-package)
+;;; Commentary:
+;; ------------
+;; We'll use org-babel from here-on-out.
 
-(use-package evil
-  :ensure t
-  :config
-  (evil-mode))
+;;; Code:
+;; ------
+
+(let ((gc-cons-threshold most-positive-fixnum))
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+	(bootstrap-version 5))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+	  (url-retrieve-synchronously
+	   "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	   'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+
+  (require 'straight)
+  (straight-use-package 'org)
+  (require 'org)
+
+  (org-babel-load-file
+   (expand-file-name "config.org" user-emacs-directory))
+
+  (garbage-collect))
+
+(provide 'init.el)
+;;; init.el ends here
