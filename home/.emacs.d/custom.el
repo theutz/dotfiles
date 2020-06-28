6bee65b802fa9fd1772285e47c8db22855ebc236
Michael Utz <michael@theutz.com>
2020-06-28 21:56:10 +0300

    Update org keymaps

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/org/inbox.org")))
+ '(org-agenda-files
+   (quote
+    ("~/.dotfiles/home/.emacs.d/config.org" "~/org/inbox.org")))
  '(org-capture-templates nil)
  '(org-directory "/Users/theutz/org"))
 (custom-set-faces
