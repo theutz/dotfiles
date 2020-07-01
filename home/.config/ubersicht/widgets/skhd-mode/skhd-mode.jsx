4aa63bc706371f34f42f7e140d6fd0cfd63e845f
Michael Utz <michael@theutz.com>
2020-07-02 00:13:53 +0300

    Make everything with yabai and skhd and uebersicht better

--- home/.config/ubersicht/widgets/skhd-mode/skhd-mode.jsx
+++ home/.config/ubersicht/widgets/skhd-mode/skhd-mode.jsx
@@ -1,42 +1,51 @@
-import { css } from "uebersicht";
+import { css, keyframes } from "uebersicht";
 
 export const command = "cat $HOME/.skhd_mode";
 
-export const refreshFrequency = 100;
+export const refreshFrequency = false;
 
-const boundary = css`
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  opacity: 0.8;
-  position: fixed;
-  border: 2px solid red;
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-`;
+export const initialState = {
+  output: "default"
+};
 
 const heading = css`
-  color: red;
+  color: white;
   font-family: "Haskplex Nerd";
   text-transform: uppercase;
   line-height: 0;
   font-size: 12px;
 `;
 
-const initialState = {
-  output: "default"
-};
-
 export const render = ({ output, error }) => {
-  if (output.trim() === "default") {
-    return <div>Hes</div>;
-  }
+  const mode = output.trim()
+
+  if (mode === 'default') return
+
+  const color = ['normal'].includes(mode)
+              ? '#3FEEE6'
+              : ['prev', 'next'].includes(mode)
+              ? '#55BCC9'
+              : ['space'].includes(mode)
+              ? '#97CAEF'
+	      : ['delete', 'insert'].includes(mode)
+	      ? '#FC4445'
+	      : '#CAFAFE'
+
+  const boundary = css`
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    position: fixed;
+    border: 5px solid ${color};
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+  `;
 
   return (
     <div className={boundary}>
-      <h1 className={heading}>{output}</h1>
+      <h1 className={heading} key={mode}>{mode}</h1>
     </div>
   );
 };
