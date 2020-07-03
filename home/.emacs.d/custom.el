a91b3a2236d9482818d7d54b1ebb8cbf7ef59610
Michael Utz <michael@theutz.com>
2020-07-03 12:47:49 +0300

    Getting emacs even better and such

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -3,13 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(evil-split-window-below t)
  '(org-agenda-files
    (quote
-    ("~/.dotfiles/home/.emacs.d/config.org" "~/org/inbox.org")))
- '(org-capture-templates nil)
- '(org-directory "/Users/theutz/org")
- '(web-mode-code-indent-offset 2))
+    ("~/.dotfiles/home/.emacs.d/config.org" "~/org/inbox.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
