4f4303e3d2d7ef8b6bd0714b311b7e2033f226db
Michael Utz <michael@theutz.com>
2020-07-05 23:10:50 +0300

    Make things a bit better in emacsville

--- home/.emacs.d/lisp/config.el
+++ home/.emacs.d/lisp/config.el
@@ -17,14 +17,7 @@
 (require 'utz-macros)
 (require 'utz-funcs)
 (require 'project-setup)
-
-(use-package sh-script
-  :mode (("\\.zpreztorc\\'" . sh-mode)
-	 ("\\zpreztorc\\'" . sh-mode)
-	 ("\\.zprofile\\'" . sh-mode)
-	 ("\\zprofile\\'" . sh-mode)
-	 ("\\zshenv\\'" . sh-mode)
-	 ("\\zshrc\\'" . sh-mode)))
+(require 'other-packages)
 
 (setq-default flycheck-emacs-lisp-load-path 'inherit)
 (setq custom-file utz-custom-file)
@@ -62,109 +55,6 @@
 
 (setq-default indent-tabs-mode 2)
 (setq-default tab-width 2)
-
-(use-package yasnippet
-  :init
-  (declare-function yas-global-mode "yasnippet")
-  :config
-  (setq yas-snippet-dirs '(utz-snippets-directory)
-	yas-verbosity 2)
-  (yas-global-mode 1))
-
-(use-package git-gutter
-  :init
-  (declare-function global-git-gutter-mode "git-gutter")
-  :config
-  (global-git-gutter-mode +1))
-
-(use-package lsp-mode
-  :init (setq lsp-keymap-prefix "s-l")
-  :hook ((lsp-mode . lsp-enable-which-key-integration)
-	 (web-mode . lsp-mode))
-  :commands (lsp))
-
-(use-package lsp-ui
-  :commands (lsp-ui-mode)
-  :config
-  (add-to-list 'lsp-ui-doc-frame-parameters '(no-accept-focus . t)))
-
-(use-package helm-lsp
-  :commands (helm-lsp-workspace-symbol))
-
-(use-package lsp-treemacs
-  :commands lsp-treemacs-errors-list)
-
-(use-package web-mode
-  :init
-  (setq web-mode-code-indent-offset 2)
-  :mode ("\\.html?\\'"
-	 "\\.jsx?\\'"
-	 "\\.tsx?\\'"
-	 "\\.json\\'"))
-
-(use-package vterm
-  :after evil
-  :init
-  (setq vterm-kill-buffer-on-exit t)
-  :config
-  (add-hook 'vterm-mode-hook (lambda ()
-			       (setq-local evil-insert-state-cursor 'box)
-			       (evil-insert-state))))
-
-(use-package multi-vterm
-  :after vterm)
-
-(use-package flycheck
-  :functions global-flycheck-mode
-  :after (exec-path-from-shell)
-  :config
-  (global-flycheck-mode))
-
-(use-package exec-path-from-shell
-  :init
-  (declare-function exec-path-from-shell-initialize "exec-path-from-shell")
-  :config
-  (exec-path-from-shell-initialize))
-
-(use-package restart-emacs)
-
-(use-package company
-  :hook (after-init . global-company-mode))
-
-(use-package company-box
-  :hook (company-mode . company-box-mode))
-
-(use-package which-key
-  :demand t
-  :init
-  (declare-function which-key-setup-side-window-bottom "which-key")
-  (declare-function which-key-mode "which-key")
-  :config
-  (which-key-setup-side-window-bottom)
-  (which-key-mode))
-
-(use-package prettier-js
-  :hook (web-mode . prettier-js-mode))
-
-(use-package all-the-icons)
-
-(use-package helpful)
-
-(use-package finder+)
-
-(use-package doom-themes
-  :functions (doom-themes-visual-bell-config
-	     doom-themes-org-config
-	     doom-themes-treemacs-config)
-  :config
-  (setq doom-themes-enable-bold t
-	doom-themes-enable-italic t)
-
-  (load-theme 'doom-one t)
-
-  (doom-themes-visual-bell-config)
-  (doom-themes-org-config)
-  (doom-themes-treemacs-config))
 
 (general-define-key "s-q" 'utz/delete-frame
 		    "C-q" 'kill-emacs)
@@ -226,13 +116,6 @@
   :states '(normal insert)
   :keymaps '(org-mode-map)
   "'" '(org-edit-special :wk "Edit Special"))
-
-(add-hook 'org-src-mode-hook
-	  (lambda ()
-	    (utz/set-localleader-key
-	      :states '(normal)
-	      :keymap 'org-src-mode-map
-	      "'" '(org-edit-src-exit :wk "Exit"))))
 
 (general-create-definer utz/set-help-key
   :prefix "C-h")
--- home/.emacs.d/lisp/org-init.el
+++ home/.emacs.d/lisp/org-init.el
@@ -10,32 +10,39 @@
 (require 'straight-init)
 
 (use-package org
-  :defines org-capture-templates
-  :general
-  (utz/set-leader-key/o "c" '(org-capture :wk "Capture"))
-  (utz/set-leader-key/o-a "a" '(org-agenda :wk "Agenda"))
-  (utz/set-leader-key/o-r
-    "r" '(helm-org-rifle :wk "Open Buffers")
-    "R" '(helm-org-rifle-occur :wk "Open Buffers (Occur)")
-    "a" '(helm-org-rifle-agenda-files :wk "Agenda Files")
-    "A" '(helm-org-rifle-occur-agenda-files :wk "Agenda Files (Occur)")
-    "b" '(helm-org-rifle-current-buffer :wk "Current Buffer")
-    "B" '(helm-org-rifle-occur-current-buffer :wk "Current Buffer (Occur)")
-    "o" '(helm-org-rifle-org-directory :wk "Org Directory")
-    "O" '(helm-org-rifle-occur-org-directory :wk "Org Directory (Occur)"))
-  :init
-  (setq org-directory (expand-file-name "~/org")
-	org-archive-location (expand-file-name "archive.org" org-directory)
-	org-default-notes-file (expand-file-name "inbox.org" org-directory)
-	org-capture-templates '(("t" "Todo" entry (file org-default-notes-file)
-				 "* TODO %?\n\t%i\n\t%a")))
-  :config
-  (require 'org-inlinetask))
+	:after general
+	:defines org-capture-templates
+	:general
+	(utz/set-leader-key/o "c" '(org-capture :wk "Capture"))
+	(utz/set-leader-key/o-a "a" '(org-agenda :wk "Agenda"))
+	(utz/set-leader-key/o-r
+		"r" '(helm-org-rifle :wk "Open Buffers")
+		"R" '(helm-org-rifle-occur :wk "Open Buffers (Occur)")
+		"a" '(helm-org-rifle-agenda-files :wk "Agenda Files")
+		"A" '(helm-org-rifle-occur-agenda-files :wk "Agenda Files (Occur)")
+		"b" '(helm-org-rifle-current-buffer :wk "Current Buffer")
+		"B" '(helm-org-rifle-occur-current-buffer :wk "Current Buffer (Occur)")
+		"o" '(helm-org-rifle-org-directory :wk "Org Directory")
+		"O" '(helm-org-rifle-occur-org-directory :wk "Org Directory (Occur)"))
+	:init
+	(setq org-directory (expand-file-name "~/org")
+				org-archive-location (expand-file-name "archive.org" org-directory)
+				org-default-notes-file (expand-file-name "inbox.org" org-directory)
+				org-capture-templates '(("t" "Todo" entry (file org-default-notes-file)
+																 "* TODO %?\n\t%i\n\t%a")))
+	:config
+	(require 'org-inlinetask)
+	(add-hook 'org-src-mode-hook
+						(lambda ()
+							(utz/set-localleader-key
+								:states '(normal)
+								:keymap 'org-src-mode-map
+								"'" '(org-edit-src-exit :wk "Exit")))))
 
 (use-package org-bullets
-  :init
-  (declare-function org-bullets-mode "org-bullets")
-  :hook (org-mode . (lambda () (org-bullets-mode 1))))
+	:init
+	(declare-function org-bullets-mode "org-bullets")
+	:hook (org-mode . (lambda () (org-bullets-mode 1))))
 
 (use-package helm-org-rifle)
 
