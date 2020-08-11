1fa7acb4cc1fa6d20d9c2fcb54dcc41043e241da
Michael Utz <michael@theutz.com>
2020-08-12 02:10:49 +0300

    Make stuff even more awesome

--- home/.config/skhd/helpers.zsh
+++ home/.config/skhd/helpers.zsh
@@ -1,17 +1,21 @@
 #!/usr/bin/env zsh
-# set -euo pipefail
 
 function skhd-refresh-widget {
+  if [[ -z "$1" ]]; then
+  else
+    echo "$1"
+  fi
+
   osascript -e "tell application id \"tracesOf.Uebersicht\" to refresh widget id \"skhd-mode-skhd-mode-jsx\""
 }
 
 function skhd-reload {
   skhd --reload
   noti -t 'skhd' -m 'Configuration Reloaded!'
-  echo "default"
-  skhd-refresh-widget
+  skhd-refresh-widget "default"
 }
 
 function skhd-escape-mode {
-    skhd -k "escape"
+  local times=`echo ${@:-1} || 1`
+  repeat $times skhd -k "escape"
 }
--- home/.config/yabai/helpers.zsh
+++ home/.config/yabai/helpers.zsh
@@ -1,49 +1,161 @@
 #!/usr/bin/env zsh
-# set -euo pipefail
 
 source $HOME/.config/skhd/helpers.zsh
+
+function yabai-cycle-window-focus-clockwise {
+    if yabai -m window --focus east; then
+    elif yabai -m display --focus next; then
+        yabai -m window --focus first
+    elif yabai -m display --focus first; then
+        yabai -m window --focus first
+    else exit 1
+    fi
+}
+
+function yabai-cycle-window-focus-counterclockwise {
+    if yabai -m window --focus west; then
+    elif yabai -m display --focus prev; then
+        yabai -m window --focus last
+    elif yabai -m display --focus last; then
+        yabai -m window --focus last
+    else exit 1
+    fi
+}
+
+function yabai-cycle-window-focus-downwards {
+    if yabai -m window --focus south; then
+    elif yabai -m window --focus next; then
+    elif yabai -m window --focus first; then
+    else exit 1
+    fi
+}
+
+function yabai-cycle-window-focus-upwards {
+    if yabai -m window --focus north; then
+    elif yabai -m window --focus prev; then
+    elif yabai -m window --focus last; then
+    else exit 1
+    fi
+}
+
+function yabai-cycle-window-focus-by-index-desc {
+    if yabai -m window --focus next; then
+    else yabai -m window --focus first
+    fi
+}
+
+function yabai-cycle-window-focus-by-index-asc {
+    if yabai -m window --focus prev; then
+    else yabai -m window --focus last
+    fi
+}
+
+function yabai-cycle-focus-to-next-space {
+    if yabai -m space --focus next; then
+    else yabai -m space --focus first
+    fi
+}
+
+function yabai-cycle-focus-to-prev-space {
+    if yabai -m space --focus prev; then
+    else yabai -m space --focus last
+    fi
+}
+
+function yabai-focus-space-by-number {
+    yabai -m space --focus $1
+}
 
 function yabai-get-window-id {
     local id=`yabai -m query --windows --window | jq '.id'`
     echo "$id"
 }
 
-function yabai-cycle-focus-clockwise {
-    if yabai -m window --focus east; then
-    elif yabai -m display --focus next; then
-        yabai -m window --focus first
+function yabai-move-current-window-to-new-space {
+    if yabai -m space --create; then
+        local new_space_index=`yabai -m query --spaces --display | jq '.[-1].index'`
+        local window_id=`yabai-get-window-id`
+        yabai -m window --space $new_space_index
+        yabai -m window --focus $window_id
+    fi
+    skhd-escape-mode
+}
+
+function yabai-get-window-ids-in-current-space {
+    local ids=`yabai -m query --windows --space | jq '.[].id'`
+    echo "$ids"
+}
+
+function yabai-destroy-current-space {
+    local current_window_id=`yabai-get-window-id`
+    for window_id in `yabai-get-window-ids-in-current-space`; do
+        yabai -m window $window_id --space prev
+    done
+    yabai -m space --destroy
+    yabai -m window --focus $current_window_id
+    skhd-escape-mode
+}
+
+function yabai-move-current-window-to-next-space {
+    local id=`yabai-get-window-id`
+    yabai -m window --space next
+    yabai -m window --focus $id
+}
+
+function yabai-move-current-window-to-prev-space {
+    local id=`yabai-get-window-id`
+    yabai -m window --space prev
+    yabai -m window --focus $id
+}
+
+function yabai-move-current-window-to-space-by-index {
+    local id=`yabai-get-window-id`
+    yabai -m window --space $1
+    yabai -m window --focus $id
+    skhd-escape-mode
+}
+
+function yabai-cycle-focus-to-next-display {
+    if yabai -m display --focus next; then
     elif yabai -m display --focus first; then
-        yabai -m window --focus first
-    else
-        exit 1
+    else exit 1
     fi
 }
 
-function yabai-cycle-focus-counterclockwise {
-    if yabai -m window --focus west; then
-    elif yabai -m display --focus prev; then
-        yabai -m window --focus last
+function yabai-cycle-focus-to-prev-display {
+    if yabai -m display --focus prev; then
     elif yabai -m display --focus last; then
-        yabai -m window --focus last
-    else
-        exit 1
+    else exit 1
     fi
 }
 
-function yabai-cycle-focus-downwards {
-    if yabai -m window --focus south; then
-    elif yabai -m window --focus next; then
-    elif yabai -m window --focus first; then
-    else
-        exit 1
-    fi
+function yabai-move-current-window-to-next-display {
+    local id=`yabai-get-window-id`
+    yabai -m window --display next
+    yabai -m window --focus $id
+    skhd-escape-mode
 }
 
-function yabai-cycle-focus-upwards {
-    if yabai -m window --focus north; then
-    elif yabai -m window --focus prev; then
-    elif yabai -m window --focus last; then
+function yabai-move-current-window-to-prev-display {
+    local id=`yabai-get-window-id`
+    yabai -m window --display prev
+    yabai -m window --focus $id
+    skhd-escape-mode
+}
+
+function yabai-toggle-mouse-autofocus {
+    case `yabai -m config focus_follows_mouse` in
+        'disabled')
+            yabai -m config focus_follows_mouse autoraise
+            ;;
+        *)
+            yabai -m config focus_follows_mouse off
+            ;;
+    esac
+
+    if [[ `yabai -m config mouse_follows_focus` == 'on' ]]; then
+        yabai -m config mouse_follows_focus off
     else
-        exit 1
+        yabai -m config mouse_follows_focus on
     fi
 }
