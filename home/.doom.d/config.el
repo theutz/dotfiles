2c761640a4bc26661318b61b6ef68cc9f9b017f2
Michael Utz <michael@theutz.com>
2020-08-29 18:42:06 +0300

    Remove kitty shortcut

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -53,8 +53,6 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(map! :leader :desc "Open in Kitty" "o k" '+macos/open-in-kitty)
-
 (defun my-open-calendar ()
   (interactive)
   (cfw:open-calendar-buffer
