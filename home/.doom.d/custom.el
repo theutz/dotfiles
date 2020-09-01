18aa87df1c93b82ea0a4daeb4c9c2377280a7838
Michael Utz <michael@theutz.com>
2020-09-01 14:56:35 +0300

    [doom-emacs] Change date style to european

--- home/.doom.d/custom.el
+++ home/.doom.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(calendar-date-style 'european)
  '(diary-file (expand-file-name "diary.org" org-directory))
  '(evil-split-window-below t)
  '(evil-vsplit-window-right t)
