5d3211a88dd60e6798797b8a388e0e2ec8e02dca
Michael Utz <michael@theutz.com>
2020-09-10 08:45:42 +0300

    [doom-emacs] Fix org directory variables

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -64,7 +64,7 @@
 ;; org-journal files
 (add-to-list 'auto-mode-alist '("journal/[0-9]\\\{8\\\}\\'" . org-journal-mode))
 (setq org-agenda-file-regexp "\\`\\\([^.].*\\.org\\\|[0-9]\\\{8\\\}\\\(\\.gpg\\\)?\\\)\\'"
-      org-agenda-files `(,org-directory ,org-journal-dir))
+      org-agenda-files `(,(expand-file-name org-directory) ,(expand-file-name org-journal-dir org-directory)))
 
 ;; Config ZSH Files
 (let ((config-files-list '("\\.?zshenv\\'"
