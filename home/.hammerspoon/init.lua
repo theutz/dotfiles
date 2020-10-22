3dc4b3e472b7d7f9dd75c749a4ee8e533d655f3b
Michael Utz <michael@theutz.com>
2020-10-22 17:03:08 +0300

    Reload config with hyper + r

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -2,10 +2,12 @@ hs.loadSpoon('SpoonInstall')
 Install = spoon.SpoonInstall
 Install:updateRepo()
 
-local hammer = {"ctrl", "alt", "cmd"}
+local hyper = {"ctrl", "alt", "cmd", "shift"}
 
 Install:andUse('ReloadConfiguration', {
   start = true,
   hotkeys = {
-    reloadConfiguration = {hammer, "r" }}
+    reloadConfiguration = {hyper, "r" }}
   })
+
+hs.alert.show("Configuration Reloaded!")
