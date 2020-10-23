d6cceec5a60d64f7c5c0adffa08e271466005748
Michael Utz <michael@theutz.com>
2020-10-23 12:45:49 +0300

    Add more bindings

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,7 +1,9 @@
+-- Setup Spoon Install
 hs.loadSpoon('SpoonInstall')
 Install = spoon.SpoonInstall
 Install:updateRepo()
 
+-- Hyper Keybindings
 local hyper = {"ctrl", "alt", "cmd", "shift"}
 
 Install:andUse('ReloadConfiguration', {
@@ -9,5 +11,18 @@ Install:andUse('ReloadConfiguration', {
   hotkeys = {
     reloadConfiguration = {hyper, "r" }}
   })
+
+-- Other Hotkeys to bind
+hs.hotkey.bindSpec({hyper, "c"}, "Visual Studio Code", function ()
+  hs.application.launchOrFocus("Visual Studio Code")
+end)
+
+hs.hotkey.bindSpec({hyper, "z"}, "Zoom", function ()
+  hs.application.launchOrFocus("Zoom.us")
+end)
+
+hs.hotkey.bindSpec({hyper, "s"}, "Spotify", function ()
+  hs.application.launchOrFocus("Spotify")
+end)
 
 hs.alert.show("Configuration Reloaded!")
