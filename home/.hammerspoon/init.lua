812a1b760b5eb7497794178cff1ee3068055b3d6
Michael Utz <michael@theutz.com>
2020-10-23 14:43:07 +0300

    More application key bindings

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -13,7 +13,7 @@ Install:andUse('ReloadConfiguration', {
   })
 
 -- Other Hotkeys to bind
-hs.hotkey.bindSpec({hyper, "c"}, "Visual Studio Code", function ()
+hs.hotkey.bindSpec({hyper, "v"}, "Visual Studio Code", function ()
   hs.application.launchOrFocus("Visual Studio Code")
 end)
 
@@ -23,6 +23,38 @@ end)
 
 hs.hotkey.bindSpec({hyper, "s"}, "Spotify", function ()
   hs.application.launchOrFocus("Spotify")
+end)
+
+hs.hotkey.bindSpec({hyper, "c"}, "Fantastical", function ()
+  hs.application.launchOrFocus("Fantastical")
+end)
+
+hs.hotkey.bindSpec({hyper, "g"}, "Google Chrome", function ()
+  hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bindSpec({hyper, "a"}, "Airmail", function ()
+  hs.application.launchOrFocus("Airmail")
+end)
+
+hs.hotkey.bindSpec({hyper, "l"}, "Slack", function ()
+  hs.application.launchOrFocus("Slack")
+end)
+
+hs.hotkey.bindSpec({hyper, "o"}, "OmniFocus", function ()
+  hs.application.launchOrFocus("OmniFocus")
+end)
+
+hs.hotkey.bindSpec({hyper, "d"}, "Dash", function ()
+  hs.application.launchOrFocus("Dash")
+end)
+
+hs.hotkey.bindSpec({hyper, "e"}, "TextExpander", function ()
+  hs.application.launchOrFocus("TextExpander")
+end)
+
+hs.hotkey.bindSpec({hyper, "i"}, "iTerm", function ()
+  hs.application.launchOrFocus("iTerm")
 end)
 
 hs.alert.show("Configuration Reloaded!")
