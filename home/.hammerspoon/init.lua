234b130130bccc1a4a7b4399c6b4dab71b687ec3
Michael Utz <michael@theutz.com>
2020-10-30 15:53:58 +0200

    [hammerspoon] Simplify bindings

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,10 +1,14 @@
 -- Setup Spoon Install
+
 hs.loadSpoon('SpoonInstall')
 Install = spoon.SpoonInstall
 Install:updateRepo()
 
--- Hyper Keybindings
+-- Basic Definitions
+
 local hyper = {"ctrl", "alt", "cmd", "shift"}
+
+-- Reload Configuration
 
 Install:andUse('ReloadConfiguration', {
                  start = true,
@@ -19,49 +23,54 @@ Install:andUse('ReloadConfiguration', {
                  },
 })
 
+-- Window Management
+
+Install:andUse('WinWin', {
+                 fn = function (WinWin)
+                   hs.window.animationDuration = 0
+
+                   local hammer = {"ctrl", "alt", "cmd"}
+                   local moves = {
+                     {"halfleft", "h"},
+                     {"halfdown", "j"},
+                     {"halfup", "k"},
+                     {"halfright", "l"},
+                     {"maximize", "return"}
+                   }
+
+                   for i = 1, #moves do
+                     local move,key = table.unpack(moves[i])
+
+                     hs.hotkey.bindSpec({hammer, key}, nil, function ()
+                         WinWin:moveAndResize(move)
+                     end)
+                   end
+                 end
+})
+
 -- Other Hotkeys to bind
-hs.hotkey.bindSpec({hyper, "v"}, "Visual Studio Code", function ()
-  hs.application.launchOrFocus("Visual Studio Code")
-end)
 
-hs.hotkey.bindSpec({hyper, "z"}, "Zoom", function ()
-  hs.application.launchOrFocus("Zoom.us")
-end)
+local appShortcuts = {
+  {"Zoom.us", "z"},
+  {"Spotify", "m"},
+  {"Fantastical", "f"},
+  {"Google Chrome", "g"},
+  {"Airmail", "a"},
+  {"Emacs", "e"},
+  {"TextExpander", "t"},
+  {"iTerm", "i"},
+  {"Slack", "s"},
+  {"Dash", "d"}
+}
 
-hs.hotkey.bindSpec({hyper, "s"}, "Spotify", function ()
-  hs.application.launchOrFocus("Spotify")
-end)
+for i = 1, #appShortcuts do
+  local app,key = table.unpack(appShortcuts[i])
 
-hs.hotkey.bindSpec({hyper, "c"}, "Fantastical", function ()
-  hs.application.launchOrFocus("Fantastical")
-end)
+  hs.hotkey.bindSpec({hyper, key}, app, function ()
+      hs.application.launchOrFocus(app)
+  end)
+end
 
-hs.hotkey.bindSpec({hyper, "g"}, "Google Chrome", function ()
-  hs.application.launchOrFocus("Google Chrome")
-end)
-
-hs.hotkey.bindSpec({hyper, "a"}, "Airmail", function ()
-  hs.application.launchOrFocus("Airmail")
-end)
-
-hs.hotkey.bindSpec({hyper, "l"}, "Slack", function ()
-  hs.application.launchOrFocus("Slack")
-end)
-
-hs.hotkey.bindSpec({hyper, "o"}, "OmniFocus", function ()
-  hs.application.launchOrFocus("OmniFocus")
-end)
-
-hs.hotkey.bindSpec({hyper, "d"}, "Dash", function ()
-  hs.application.launchOrFocus("Dash")
-end)
-
-hs.hotkey.bindSpec({hyper, "e"}, "TextExpander", function ()
-  hs.application.launchOrFocus("TextExpander")
-end)
-
-hs.hotkey.bindSpec({hyper, "i"}, "iTerm", function ()
-  hs.application.launchOrFocus("iTerm")
-end)
+-- Report Success!!
 
 hs.alert.show("Configuration Reloaded!")
