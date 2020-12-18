e1f11d7ccbe397974cde1ea3825e3462ca6c5f84
Michael Utz <michael@theutz.com>
2020-12-18 11:21:39 +0200

    [doom] add ligatures

--- home/.doom.d/init.el
+++ home/.doom.d/init.el
@@ -35,7 +35,7 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
        indent-guides     ; highlighted indent columns
-       ;;(ligatures +hasklig)       ; ligatures or substitute text with pretty symbols
+       (ligatures +hasklig)       ; ligatures or substitute text with pretty symbols
        minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
