64348b9047cfa5bbbbc9e0c75da00a1bd5357bb8
Michael Utz <michael@theutz.com>
2021-01-05 16:44:24 +0200

    [ubar] more styling improvements

--- ubersicht/widgets/ubar/ubar.jsx
+++ ubersicht/widgets/ubar/ubar.jsx
@@ -9,21 +9,31 @@ export const className = `
     bottom: 0;
     left: 0;
     right: 0;
-    background-color: rgba(0,0,0,0.8);
-    color: rgba(255,255,255,0.8);
+    height: 30px;
+    background-color: #000000;
+    color: #ccc;
     font-family: "SF Mono", monospace;
+    font-size: 14px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    opacity: 0.9;
 `;
 
 const Container = styled("div")`
+  height: 100%;
+  width: 100%;
   display: flex;
   flex-flow: row wrap;
   justify-content: center;
   align-items: center;
-  padding: 5px;
+`;
+
+const RightContainer = styled(Container)`
+  justify-content: flex-start;
 `;
 
 const Section = styled("div")`
-  border-color: #ffffff88;
+  border-color: #888;
   border-width: 2px;
   padding: 0 0.5em;
 
@@ -34,14 +44,17 @@ const Section = styled("div")`
   }
 `;
 
+const Label = styled("span")`
+  padding-right: 0.5em;
+  color: #888;
+`;
+
 const Item = styled("span")`
-  padding: 1px ${(props) => (props.compact ? "0" : "6px")};
+  padding: 1px ${(props) => (props.compact ? "2px" : "6px")};
   border-style: solid;
-  border-color: ${(props) => (props.highlight ? "#FF000088" : "transparent")};
+  border-color: ${(props) => (props.highlight ? "#ff0000" : "transparent")};
   border-width: ${(props) => (props.compact ? "0" : "1px")};
-  background-color: ${(props) =>
-    props.highlight ? "#FF000088" : "transparent"};
-  text-shadow: 1px 1px 3px #000000ff;
+  background-color: ${(props) => (props.highlight ? "#660000" : "transparent")};
 `;
 
 const Error = ({ children }) => <Container>ERROR: {children}</Container>;
@@ -49,13 +62,9 @@ const Error = ({ children }) => <Container>ERROR: {children}</Container>;
 const range = (count) => Array.from({ length: count }).map((_, i) => i + 1);
 
 const Mode = ({ mode }) => {
-  if (!mode) return null;
+  if (!mode || mode === "Default") return null;
 
-  return (
-    <Section>
-      Mode: {<Item highlight={mode !== "Default"}>{mode}</Item>}
-    </Section>
-  );
+  return <Section>{<Item highlight>{mode}</Item>}</Section>;
 };
 
 const Space = ({ space: { index, count } }) => {
@@ -65,7 +74,7 @@ const Space = ({ space: { index, count } }) => {
 
   return (
     <Section>
-      Space:{" "}
+      <Label>Space</Label>
       {spaceArr.map((x) => (
         <Item key={x} highlight={x === index}>
           {x}
@@ -82,7 +91,7 @@ const Display = ({ display: { count, index } }) => {
 
   return (
     <Section>
-      Display:{" "}
+      <Label>Display</Label>
       {displayArr.map((x) => (
         <Item key={x} highlight={x === index}>
           {x}
@@ -97,7 +106,7 @@ const Window = ({ window }) => {
 
   const { split, zoom, floating, sticky } = window;
 
-  const splitIcon = split === "horizontal" ? "↔" : "↕";
+  const splitIcon = split === "horizontal" ? "↔️" : "↕️";
   const zoomIcon = (() => {
     switch (zoom) {
       case "none":
@@ -116,8 +125,11 @@ const Window = ({ window }) => {
   return (
     <>
       <Section>
-        Window: <Item compact>{splitIcon}</Item> <Item compact>{zoomIcon}</Item>{" "}
-        {<Item compact>{floatIcon}</Item>} {<Item compact>{stickyIcon}</Item>}
+        <Label>Window</Label>
+        <Item compact>{splitIcon}</Item>
+        <Item compact>{zoomIcon}</Item>
+        <Item compact>{floatIcon}</Item>
+        <Item compact>{stickyIcon}</Item>
       </Section>
     </>
   );
@@ -141,11 +153,15 @@ export const render = ({ output, error }) => {
   const { space = {}, display = {}, skhd = {}, window } = data;
 
   return (
-    <Container>
-      <Mode mode={skhd.mode} />
-      <Display display={display} />
-      <Space space={space} />
-      <Window window={window} />
-    </Container>
+    <>
+      <RightContainer>
+        <Mode mode={skhd.mode} />
+      </RightContainer>
+      <Container>
+        <Display display={display} />
+        <Space space={space} />
+        <Window window={window} />
+      </Container>
+    </>
   );
 };
