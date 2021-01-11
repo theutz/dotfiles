fd498d963c54f06f2f3cc94366b4d29369e3903e
Michael Utz <michael@theutz.com>
2021-01-11 13:38:48 +0200

    [doom] smaller font size

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -21,8 +21,8 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 (load! "lisp/fonts")
-(setq doom-font (font-spec :family theutz-font :size 14 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family theutz-font :size 14))
+(setq doom-font (font-spec :family theutz-font :size 13 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family theutz-font :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
