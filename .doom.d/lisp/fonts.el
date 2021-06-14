8071f98cf0a4357d8d6d128d6d8ac460f352889b
Michael Utz <michael@theutz.com>
2021-06-14 13:12:24 +0300

    [emacs] Install emacs on bootstrap

--- .doom.d/lisp/fonts.el
+++ .doom.d/lisp/fonts.el
@@ -1,10 +1,7 @@
 (load! "util")
 
 (defconst theutz-fonts
-  '("BlexMono Nerd Font"
-    "Hasklug Nerd Font"
-    "Monoid Nerd Font"
-    "RobotoMono Nerd Font")
+  '("MesloLGS NF")
   "List of fonts that will be randomly selected from at startup.")
 
 (defconst theutz-font (theutz/pick-random theutz-fonts)
