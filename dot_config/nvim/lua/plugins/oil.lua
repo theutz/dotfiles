fea1e056ce2358356aa855780dae23944d726f67
Michael Utz <michael@theutz.com>
2023-06-02 19:11:21 +0300

    Update .config/nvim/lua/plugins/oil.lua

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,8 +1,19 @@
 return {
   {
     "stevearc/oil.nvim",
+    name = "oil",
     dependencies = {
       "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+    keys = {
+      {
+        "<leader>fo",
+        function()
+          require("oil").open()
+        end,
+        desc = "Oil",
+      },
     },
   },
 }
