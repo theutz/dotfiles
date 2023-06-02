38df62e903a19ca4864eeb4b7306a25f55166b84
Michael Utz <michael@theutz.com>
2023-06-02 11:21:44 +0300

    Remove .config/nvim/LICENSE Remove .config/nvim/README.md Remove .config/nvim/.gitignore Remove .config/nvim/.neoconf.json Remove .config/nvim/ftplugin/markdown.lua Remove .config/nvim/lua/config/autocmds.lua Remove .config/nvim/lua/config/keymaps.lua Remove .config/nvim/lua/config/lazy.lua Remove .config/nvim/lua/config/options.lua Remove .config/nvim/lua/plugins/alpha.lua Remove .config/nvim/lua/plugins/blade.lua Remove .config/nvim/lua/plugins/colorschemes.lua Remove .config/nvim/lua/plugins/editorconfig.lua Remove .config/nvim/lua/plugins/example.lua Remove .config/nvim/lua/plugins/focus.lua Remove .config/nvim/lua/plugins/git-conflict.lua Remove .config/nvim/lua/plugins/glow.lua Remove .config/nvim/lua/plugins/incline.lua Remove .config/nvim/lua/plugins/lazyvim.lua Remove .config/nvim/lua/plugins/lsp.lua Remove .config/nvim/lua/plugins/lualine.lua Remove .config/nvim/lua/plugins/luasnip.lua Remove .config/nvim/lua/plugins/mini.lua Remove .config/nvim/lua/plugins/moveline.lua Remove .config/nvim/lua/plugins/neotree.lua Remove .config/nvim/lua/plugins/null-ls.lua Remove .config/nvim/lua/plugins/octo.lua Remove .config/nvim/lua/plugins/oil.lua Remove .config/nvim/lua/plugins/sessions.lua Remove .config/nvim/lua/plugins/telescope.lua Remove .config/nvim/lua/plugins/tmux.lua Remove .config/nvim/lua/plugins/treesitter.lua Remove .config/nvim/lua/plugins/which-key.lua Remove .config/nvim/lua/plugins/winshift.lua Remove .config/nvim/luasnip/htmldjango.lua Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/stylua.toml Update .config/zsh/.zprofile

--- dot_config/exact_nvim/exact_ftplugin/markdown.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set("n", "<leader>cp", "<Cmd>Glow<CR>", {
-  desc = "Preview in Glow",
-  buffer = 0,
-})
--- dot_config/exact_nvim/exact_lua/exact_plugins/which-key.lua
+++
@@ -1,9 +0,0 @@
-local wk = require("which-key")
-
--- wk.register({
---   ["<leader>"] = {
---     o = { name = "open" },
---   },
--- })
---
-return {}
