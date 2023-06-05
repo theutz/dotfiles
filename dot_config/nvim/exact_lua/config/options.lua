892b5126d65fce0ca69a67fce1b5999c080ecaa3
Michael Utz <michael@theutz.com>
2023-06-05 11:51:52 +0300

    Update .Brewfile Remove .config/nvim/lua/config/neovide.lua Update .config/nvim/lua/config/options.lua Update .config/zsh/.zprofile

--- dot_config/nvim/exact_lua/config/options.lua
+++ dot_config/nvim/exact_lua/config/options.lua
@@ -1,7 +1,12 @@
--- Options are automatically loaded before lazy.nvim startup
+--
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
 if vim.g.neovide then
-  require("config.neovide")
+  vim.o.guifont = "Lexsa:h14"
+  vim.o.linespace = 0
+  vim.g.neovide_scale_factor = 1.2
+
+  vim.g.neovide_transparency = 0.5
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
 end
