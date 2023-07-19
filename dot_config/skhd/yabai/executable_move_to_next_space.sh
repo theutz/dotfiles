942f39502cc89e749e6e3f24a456a9708d4510eb
Michael Utz <michael@theutz.com>
2023-07-19 11:15:44 +0300

    Update .config/skhd/skhdrc Add .config/skhd/yabai/move_to_next_space.sh Update .config/skhd/yabai/move_to_prev_space.sh Add .config/yabai/config Update .config/yabai/yabairc Update .config/yabai/rules Change attributes of .config/skhd/yabai/focus_next_space.sh Change attributes of .config/skhd/yabai/focus_prev_space.sh

---
+++ dot_config/skhd/yabai/executable_move_to_next_space.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+win="$(yabai -m query --windows --window | jq '.id')"
+
+if [[ "$(yabai -m query --spaces --display | jq '. | length')" == "1" ]]; then
+  yabai -m space --create
+fi
+
+if ! yabai -m window --space next; then
+  yabai -m window --space first
+fi
+
+yabai -m window --focus $win
--- dot_config/skhd/yabai/executable_move_to_prev_space.sh
+++ dot_config/skhd/yabai/executable_move_to_prev_space.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ "$(yabai -m query --spaces --display | jq '. | length')" == "1" ]]; then
+  yabai -m space --create
+fi
+
 if yabai -m window --space prev; then
   yabai -m space --focus prev
 elif yabai -m window --space last; then
