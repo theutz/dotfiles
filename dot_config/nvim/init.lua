8592875a942fb78d40a9db5e74c164c1ca825553
Michael Utz <michael@theutz.com>
2023-07-20 10:56:49 +0300

    Remove .config/nvim/LICENSE Remove .config/nvim/README.md Remove .config/nvim/.gitignore Remove .config/nvim/.neoconf.json Remove .config/nvim/ftplugin/markdown.lua Remove .config/nvim/lua/config/autocmds.lua Remove .config/nvim/lua/config/keymaps.lua Remove .config/nvim/lua/config/lazy.lua Remove .config/nvim/lua/config/options.lua Remove .config/nvim/lua/plugins/alpha.lua Remove .config/nvim/lua/plugins/blade.lua Remove .config/nvim/lua/plugins/colorschemes.lua Remove .config/nvim/lua/plugins/editorconfig.lua Remove .config/nvim/lua/plugins/example.lua Remove .config/nvim/lua/plugins/focus.lua Remove .config/nvim/lua/plugins/git-conflict.lua Remove .config/nvim/lua/plugins/glow.lua Remove .config/nvim/lua/plugins/incline.lua Remove .config/nvim/lua/plugins/lazyvim.lua Remove .config/nvim/lua/plugins/lsp.lua Remove .config/nvim/lua/plugins/lualine.lua Remove .config/nvim/lua/plugins/luasnip.lua Remove .config/nvim/lua/plugins/mini.lua Remove .config/nvim/lua/plugins/moveline.lua Remove .config/nvim/lua/plugins/neotree.lua Remove .config/nvim/lua/plugins/noice.lua Remove .config/nvim/lua/plugins/null-ls.lua Remove .config/nvim/lua/plugins/oil.lua Remove .config/nvim/lua/plugins/sessions.lua Remove .config/nvim/lua/plugins/telescope.lua Remove .config/nvim/lua/plugins/tmux.lua Remove .config/nvim/lua/plugins/treesitter.lua Remove .config/nvim/lua/plugins/trouble.lua Remove .config/nvim/lua/plugins/winshift.lua Remove .config/nvim/luasnip/htmldjango.lua Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/stylua.toml

--- dot_config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
-vim.filetype.add({
-  pattern = {
-    [".*%.antlers.html"] = { "antlers.html", priority = math.huge },
-  },
-})
