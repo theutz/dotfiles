0ffa96223a6594853c0a55834f571a8c700b2ff4
Michael Utz <michael@theutz.com>
2023-08-02 13:52:35 +0300

    Update .config/nvim/lua/plugins/lazyvim.lua Update .config/nvim/lua/plugins/neo-tree.lua Update .config/nvim/lua/plugins/oil.lua Update .config/nvim/lazy-lock.json Update .config/iterm2/com.googlecode.iterm2.plist

--- dot_config/exact_nvim/exact_lua/exact_plugins/neo-tree.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/neo-tree.lua
@@ -1,9 +1,15 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>fe", false },
+    },
     opts = {
       filesystem = {
         hijack_netrw_behavior = "disabled",
+        filtered_items = {
+          hide_dotfiles = false,
+        },
       },
     },
   },
--- dot_config/exact_nvim/exact_lua/exact_plugins/oil.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/oil.lua
@@ -11,6 +11,13 @@ return {
         end,
         desc = "Open parent directory",
       },
+      {
+        "<leader>fe",
+        function()
+          require("oil").open()
+        end,
+        desc = "Open parent directory",
+      },
     },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
