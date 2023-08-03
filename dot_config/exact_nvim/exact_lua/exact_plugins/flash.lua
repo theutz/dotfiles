fc92545b3af374322b29f1b004d817929479e049
Michael Utz <michael@theutz.com>
2023-08-03 13:13:50 +0300

    Add .config/nvim/lua/plugins/flash.lua Add .config/nvim/lua/plugins/tokyonight.lua Add .config/nvim/lua/util/.keep

---
+++ dot_config/exact_nvim/exact_lua/exact_plugins/flash.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/flash.nvim",
+    keys = {
+      { "s", false },
+      { "S", false },
+    },
+    opts = {
+      modes = {
+        search = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
