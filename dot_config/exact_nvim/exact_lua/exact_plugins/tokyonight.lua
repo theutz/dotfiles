f4bd881086c8da83ebd9452f4b87562a7cf7e9fc
Michael Utz <michael@theutz.com>
2023-08-04 15:28:02 +0300

    Update .config/nvim/lua/plugins/tokyonight.lua Update .config/nvim/lazy-lock.json Update .config/iterm2/com.googlecode.iterm2.plist Update .config/tmuxp/ixs.yml Update .config/zsh/.zprofile Update .nbrc

--- dot_config/exact_nvim/exact_lua/exact_plugins/tokyonight.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/tokyonight.lua
@@ -1,12 +1,5 @@
 return {
   {
     "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
   },
 }
