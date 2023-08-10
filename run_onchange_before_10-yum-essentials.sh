c57e9154a51d7f83b4d6640eb4ebd99b40813d9c
Michael Utz <michael@theutz.com>
2023-08-10 14:17:36 +0300

    Manually install age for Centos 7

--- run_onchange_before_10-yum-essentials.sh
+++ run_onchange_before_10-yum-essentials.sh
@@ -4,6 +4,11 @@ if command -v yum; then
 	sudo yum update -y
 	sudo yum install -y epel-release \
 		yum-utils \
-		wget \
 		git
+
+	# install age for encryption
+	curl --location --output age.tar.gz 'https://dl.filippo.io/age/latest?for=linux/amd64' &&
+		tar xzf age.tar.gz &&
+		mv age/age age/age-keygen ~/bin &&
+		rm -rf age.tar.gz age
 fi
