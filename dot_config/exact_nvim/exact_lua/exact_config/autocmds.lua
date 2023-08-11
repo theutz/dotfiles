23b49679a467aa4830f52ed9b3c3542fca319844
Michael Utz <michael@theutz.com>
2023-08-11 19:09:57 +0300

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
@@ -1,3 +1,24 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.cmd([[
+augroup chezmoiapply
+  au!
+  autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path "%"
+augroup end
+]])
+
+vim.cmd([[
+augroup chezmoiadd
+  au!
+  autocmd BufWritePost ~/.config/tmux/tmux.conf,~/.config/nvim/* ! chezmoi add "%"
+augroup end
+]])
+
+vim.cmd([[
+augroup tmuxsource
+  au!
+  autocmd BufWritePost ~/.config/tmux/tmux.conf ! tmux source "%"
+augroup end
+]])
