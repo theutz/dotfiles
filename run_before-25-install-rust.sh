1dd3f93916925bc28e014d1c79465650ac44220a
Michael Utz <michael@theutz.com>
2023-08-11 20:39:11 +0300

    Add rust

--- run_before-25-install-rust.sh
+++ run_before-25-install-rust.sh
@@ -4,3 +4,4 @@ echo "== Checking on rust install =="
 if [ ! -d "$HOME"/.cargo ] && [ ! -d "$HOME/.rustup" ]; then
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
+echo "== Rust installed =="
