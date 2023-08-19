5be5060a480c4d2002b1b57a7eca60b50fc5e17d
Michael Utz <michael@theutz.com>
2023-08-19 20:24:00 +0300

    Update .config/nvim/lua/plugins/tmux.lua

--- dot_config/exact_nvim/exact_lua/exact_plugins/tmux.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/tmux.lua
@@ -5,5 +5,31 @@ return {
     config = function()
       require("tmux").setup()
     end,
+    keys = {
+      {
+        "<C-h>",
+        function()
+          require("tmux").move_left()
+        end,
+      },
+      {
+        "<C-j>",
+        function()
+          require("tmux").move_down()
+        end,
+      },
+      {
+        "<C-k>",
+        function()
+          require("tmux").move_up()
+        end,
+      },
+      {
+        "<C-l>",
+        function()
+          require("tmux").move_right()
+        end,
+      },
+    },
   },
 }
