557fb4c3085584869fa840ed5981a1e49629fc03
Michael Utz <michael@theutz.com>
2023-08-21 14:30:41 +0300

    Update .config/nvim/lua/config/options.lua

--- dot_config/exact_nvim/exact_lua/exact_config/options.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/options.lua
@@ -1,6 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
--- vim.o.ttimeout = 0
--- vim.o.timeout = 0
