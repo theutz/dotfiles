0f6620b3020e5b20e707127189316b61af341883
Michael Utz <michael@theutz.com>
2023-08-21 19:13:17 +0300

    Update .config/iterm2/com.googlecode.iterm2.plist Update .config/zsh/aliases.zsh

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -16,7 +16,6 @@ alias brewpx="brew services stop"
 alias cm="chezmoi"
 alias caskk="brew info --cask"
 alias hosts="sudo nvim /etc/hosts && dscacheutil -flushcache"
-alias ls="lsd"
 alias r="ranger"
 alias ranger="source ranger"
 
