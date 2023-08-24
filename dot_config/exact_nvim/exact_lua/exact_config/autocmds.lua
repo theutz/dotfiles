2f511ed26f1491c395235fd290468c72eda1c461
Michael Utz <michael@theutz.com>
2023-08-24 12:44:46 +0300

    Update .chezmoiscripts/10-install-packages-darwin.sh Update .config/nvim/lua/config/autocmds.lua Update .config/nvim/lazy-lock.json

--- dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
@@ -3,25 +3,6 @@
 -- Add any additional autocmds here
 
 vim.cmd([[
-augroup chezmoiapply
-  au!
-  autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path "%"
-augroup end
-
-augroup chezmoiadd
-  au!
-  autocmd BufWritePost 
-    \~/.config/tmux/tmux.conf,
-    \~/.config/nvim/*,
-    \~/.config/zsh/.zprofile,
-    \~/.config/zsh/.zshenv,
-    \~/.config/zsh/.zshrc,
-    \~/.config/zsh/.zlogin,
-    \~/.config/zsh/.zlogout,
-    \~/.config/zsh/.zpreztorc
-    \ ! chezmoi add "%"
-augroup end
-
 augroup tmuxsource
   au!
   autocmd BufWritePost ~/.config/tmux/tmux.conf ! tmux source "%"
