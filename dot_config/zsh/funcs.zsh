2e0aaa9fbbc68587e9945c4beb98590d01c71021
Michael Utz <michael@theutz.com>
2023-08-25 15:28:39 +0300

    Update .config/zsh/funcs.zsh

--- dot_config/zsh/funcs.zsh
+++ dot_config/zsh/funcs.zsh
@@ -118,7 +118,7 @@ function txa {
   fi
 
   if ! tmux has-session -t "$session" &>/dev/null; then
-    tmuxp load -d "$session"
+    tmuxp load -d "$session" &>/dev/null
   fi
 
   tmux attach-session -t "$session" &>/dev/null || tmux switch-client -t "$session" &>/dev/null
