9acd1d15b4519cfca001c736f0262d399afc9804
Michael Utz <michael@theutz.com>
2023-08-29 13:22:23 +0300

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -2,13 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local wk = require("which-key")
+wk.register({
+	["<leader>#"] = { name = "Fancy Comments" },
+})
+
 vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save file" })
 
 vim.keymap.del({ "i" }, "<M-j>")
 vim.keymap.del({ "i" }, "<M-k>")
 
-local wk = require("which-key")
-wk.register({
-	["<leader>#"] = { name = "Fancy Comments" },
-})
-vim.keymap.set("n", "<leader>#a", "<cmd>.!boxes -d ada-box | boxes -d pound-cmd<cr>", { desc = "Ada Box -> Pound Cmd" })
+vim.keymap.set("n", "<leader>#a", ":.!boxes -d ada-box | boxes -d pound-cmd<cr>", { desc = "Ada Box -> Pound Cmd" })
