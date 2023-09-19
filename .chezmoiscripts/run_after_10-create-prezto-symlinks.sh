5b46dfa661c1eb81e498f770c1f81027b63542c9
Michael Utz <michael@theutz.com>
2023-09-19 16:48:35 +0300

    Update .chezmoiexternal.toml Update .chezmoiscripts/10-create-prezto-symlinks.sh Remove .tmux.conf

--- .chezmoiscripts/run_after_10-create-prezto-symlinks.sh
+++ .chezmoiscripts/run_after_10-create-prezto-symlinks.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
-for rcfile in "${ZPREZTODIR}"/runcoms/^README.md(.N); do
-  ln -sf $rcfile "${ZDOTDIR}/.${rcfile:t}"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
