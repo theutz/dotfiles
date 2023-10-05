3541cbb54cf6bd4eb301e18847e4afb721ca4997
Michael Utz <michael@theutz.com>
2023-10-05 22:37:29 +0300

    Update .chezmoiscripts/10-link-prezto-runcoms.sh Update .chezmoiscripts/50-install-tmux-plugins.sh

--- .chezmoiscripts/run_after_10-link-prezto-runcoms.sh
+++ .chezmoiscripts/run_after_10-link-prezto-runcoms.sh
@@ -8,7 +8,7 @@ for rcfile in "${ZPREZTODIR:-${ZDOTDIR:-${${XDG_CONFIG_HOME:-$HOME/.config}/zsh}
   from="$rcfile"
   to="${ZDOTDIR:-${${XDG_CONFIG_HOME:-$HOME/.config}/zsh}}/.${rcfile:t}"
 
-  printf '%s -> %s' $rcfile 
+  printf '%s -> %s\n' $from $to
 
 	ln -sf $from $to
 done
--- .chezmoiscripts/run_after_50-install-tmux-plugins.sh
+++ .chezmoiscripts/run_after_50-install-tmux-plugins.sh
@@ -5,6 +5,7 @@ printf "\nInstalling tmux plugins...\n\n"
 ~/.config/tmux/plugins/tpm/bin/install_plugins
 
 if [[ -n "$TMUX" ]]; then
+  printf "\nReloading tmux config\n\n"
   tmux source-file ~/.config/tmux/tmux.conf
   tmux display-message "#[fg=default bg=green fill=green] Plugins installed and config reloaded by #[italics]chezmoi"
 fi
