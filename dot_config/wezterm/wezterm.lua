7a5500e2d4b1312e5c5712e29834707912531fff
Michael Utz <michael@theutz.com>
2023-10-05 19:44:41 +0300

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,25 +1,34 @@
 local wezterm = require("wezterm")
-
 local config = {}
+local act = wezterm.action
 
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- Color scheme
+-- Color Scheme
 config.color_scheme = "Tokyo Night Moon"
 
 -- Tab Bar
-config.hide_tab_bar_if_only_one_tab = false
-config.tab_bar_at_bottom = true
-config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = false
+config.use_fancy_tab_bar = false
 
+-- Window
+config.quit_when_all_windows_are_closed = false
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
+config.adjust_window_size_when_changing_font_size = false
+config.initial_cols = 1000
+config.initial_rows = 1000
 
+-- Command Palette
+config.command_palette_font_size = 16
+
+-- Fonts
 config.font = wezterm.font("Lexsa")
 config.font_size = 16
-config.line_height = 1.2
+config.line_height = 1.0
 config.cell_width = 1.0
 
 return config
