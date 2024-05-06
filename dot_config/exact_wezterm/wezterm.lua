24035425666248edafab1db840839b3702156277
Michael Utz <michael@theutz.com>
2024-05-07 01:42:25 +0300

    Update .config/wezterm/wezterm.lua

--- dot_config/exact_wezterm/wezterm.lua
+++ dot_config/exact_wezterm/wezterm.lua
@@ -6,15 +6,17 @@ if wezterm.config_builder then config = wezterm.config_builder() end
 config:set_strict_mode(true)
 
 -- Load all modules defined in spec
-local module_files = wezterm.glob("{config,handlers}/*.lua", wezterm.config_dir)
-for _, file_path in ipairs(module_files) do
-	local module_name = file_path:gsub("([%w]*)/([%w-]*)%.lua", "%1.%2")
-	local ok, module = pcall(require, module_name)
-	if ok and type(module) == "table" then
-		module.setup(config)
-	else
-		wezterm.log_warn("No " .. module_name .. " module exists")
-	end
-end
+-- local module_files = wezterm.glob("{config,handlers}/*.lua", wezterm.config_dir)
+-- for _, file_path in ipairs(module_files) do
+-- 	local module_name = file_path:gsub("([%w]*)/([%w-]*)%.lua", "%1.%2")
+-- 	local ok, module = pcall(require, module_name)
+-- 	if ok and type(module) == "table" then
+-- 		module.setup(config)
+-- 	else
+-- 		wezterm.log_warn("No " .. module_name .. " module exists")
+-- 	end
+-- end
+
+config.default_prog = { "/opt/homebrew/bin/nu" }
 
 return config
