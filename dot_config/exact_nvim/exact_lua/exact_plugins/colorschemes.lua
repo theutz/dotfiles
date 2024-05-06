e3887cedbcee9071dd2c8c173adeb5f161bf8a05
Michael Utz <michael@theutz.com>
2024-05-06 23:26:49 +0300

    Add .config/nvim/.neoconf.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/neovide.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/arrow.lua Add .config/nvim/lua/plugins/better-digraphs-nvim.lua Add .config/nvim/lua/plugins/boole-nvim.lua Add .config/nvim/lua/plugins/colorschemes.lua Add .config/nvim/lua/plugins/comment-box.lua Add .config/nvim/lua/plugins/diffview-nvim.lua Add .config/nvim/lua/plugins/flash.lua Add .config/nvim/lua/plugins/gist-nvim.lua Add .config/nvim/lua/plugins/hawtkeys.lua Add .config/nvim/lua/plugins/hurl-nvim.lua Add .config/nvim/lua/plugins/iswap.lua Add .config/nvim/lua/plugins/kdl.lua Add .config/nvim/lua/plugins/lualine-nvim.lua Add .config/nvim/lua/plugins/luasnip.lua Add .config/nvim/lua/plugins/mason-nvim-dap.lua Add .config/nvim/lua/plugins/model.lua Add .config/nvim/lua/plugins/neo-tree.lua Add .config/nvim/lua/plugins/neocomposer-nvim.lua Add .config/nvim/lua/plugins/neogit.lua Add .config/nvim/lua/plugins/neorg.lua Add .config/nvim/lua/plugins/neoswap.lua Add .config/nvim/lua/plugins/neotest.lua Add .config/nvim/lua/plugins/noice.lua Add .config/nvim/lua/plugins/nushell-nvim.lua Add .config/nvim/lua/plugins/nvim-lspconfig.lua Add .config/nvim/lua/plugins/nvim-notify.lua Add .config/nvim/lua/plugins/oil.lua Add .config/nvim/lua/plugins/scratch.lua Add .config/nvim/lua/plugins/smart-splits-nvim.lua Add .config/nvim/lua/plugins/telescope.lua Add .config/nvim/lua/plugins/text-case.lua Add .config/nvim/lua/plugins/thanks-nvim.lua Add .config/nvim/lua/plugins/tmux.lua Add .config/nvim/lua/plugins/treesitter.lua Add .config/nvim/lua/plugins/unception.lua Add .config/nvim/lua/plugins/vim-blade.lua Add .config/nvim/lua/plugins/vim-easy-align.lua Add .config/nvim/lua/plugins/vim-hugo.lua Add .config/nvim/lua/plugins/vim-jinja2-syntax.lua Add .config/nvim/lua/plugins/vim-just.lua Add .config/nvim/lua/plugins/which-key.lua Add .config/nvim/lua/plugins/winshift.lua Add .config/nvim/snippets/snippets/all.json Add .config/nvim/snippets/snippets/html.json Add .config/nvim/snippets/snippets/jinja.json Add .config/nvim/snippets/package.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/neoconf.json Add .config/nvim/stylua.toml

---
+++ dot_config/exact_nvim/exact_lua/exact_plugins/colorschemes.lua
@@ -0,0 +1,150 @@
+-- vim: fdm=indent fdl=1
+local colors = {
+  {
+    "ribru17/bamboo.nvim",
+    opts = {
+      style = "multiplex",
+      transparent = true,
+      dim_inactive = true,
+      lualine = {
+        transparent = true,
+      },
+    },
+  },
+  {
+    "RRethy/base16-nvim",
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+    },
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    opts = {
+      transparent = true,
+      italic_comments = true,
+      hide_fillchars = true,
+      borderless_telescope = true,
+    },
+  },
+  {
+    "sainnhe/everforest",
+    config = function()
+      vim.g.everforest_background = "hard"
+      vim.g.everforest_transparent_background = 0 -- 0, 1, 2
+    end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    config = true,
+    opts = {
+      style = "moon",
+      transparent = true,
+      dimInactive = false,
+    },
+  },
+  {
+    "dasupradyumna/midnight.nvim",
+  },
+  {
+    "miikanissi/modus-themes.nvim",
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+    opts = {
+      options = {
+        cursorline = false,
+        transparency = true,
+        lualine_transparency = true,
+      },
+    },
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+  },
+  {
+    "sainnhe/sonokai",
+    config = function()
+      vim.g.sonokai_style = "atlantis" -- default, shusia, andromeda, atlantis, maia, espresso
+      vim.g.sonokai_better_performance = 1
+    end,
+  },
+  {
+    "ray-x/starry.nvim",
+    config = function(_, opts)
+      local s = require "starry"
+      s.setup(opts)
+      vim.g.starry_disable_background = true
+    end,
+  },
+  {
+    "projekt0n/github-nvim-theme",
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      style = "moon",
+      light_style = "night",
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    opts = {
+      options = {
+        transparent = true,
+      },
+    },
+  },
+  {
+    "tiagovla/tokyodark.nvim",
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
+    url = "https://codeberg.org/jthvai/lavender.nvim",
+    branch = "stable", -- versioned tags + docs updates from main
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "moon",
+      dark_variant = "moon",
+      styles = {
+        transparency = false,
+      },
+    },
+  },
+}
+
+local colorscheme = "rose-pine"
+
+local plugins = {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = colorscheme,
+    },
+  },
+}
+
+for _, color in pairs(colors) do
+  color.priority = 1000
+  table.insert(plugins, color)
+end
+
+return plugins
