98a0050608e02f100f2e9388412a34cc9de177e5
Michael Utz <michael@theutz.com>
2024-05-07 14:40:29 +0300

    Update .config/nvim/lua/plugins/neo-tree.lua Update .config/nvim/lua/plugins/oil.lua Update .config/nvim/lua/plugins/smart-splits-nvim.lua Add .config/nvim/lua/plugins/yazi-nvim.lua

---
+++ dot_config/exact_nvim/exact_lua/exact_plugins/yazi-nvim.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "mikavilpas/yazi.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    event = "VeryLazy",
+    config = true,
+    keys = {
+      { "<leader>e", function() require("yazi").yazi() end, desc = "Open file explorer" },
+    },
+    opts = {
+      open_for_directories = true,
+      floating_window_scaling_factor = 0.9,
+      yazi_floating_window_winblend = 90,
+    },
+  },
+}
