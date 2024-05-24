d773ebcc3fe8aa88e31c3cb8ad3d77cf8d153cad
Michael Utz <michael@theutz.com>
2024-05-24 15:51:23 +0300

    Add .config/doom/custom.el Add .config/doom/themes/doom-rose-pine-dawn-theme.el Add .config/doom/themes/doom-rose-pine-moon-theme.el Add .config/doom/themes/doom-rose-pine-theme.el

---
+++ dot_config/exact_doom/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(safe-local-variable-values
+   '((eval add-hook 'org-babel-post-tangle-hook #'chezmoi-write)
+     (line-spacing . 0.5))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
