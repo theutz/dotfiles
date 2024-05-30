900f8e131b7c9d579ec078f4e9bd9f800c40177e
Michael Utz <michael@theutz.com>
2024-05-30 11:55:23 +0300

    Update .config/doom/config.org Update .config/doom/custom.el Update .config/doom/init.el Update .config/git/config Update .config/nushell/nushell.org Update .qutebrowser/quickmarks Update .qutebrowser/bookmarks/urls

--- dot_config/doom/custom.el
+++ dot_config/doom/custom.el
@@ -5,7 +5,30 @@
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(safe-local-variable-values
-   '((evil-shift-width . 2)
+   '((eval add-hook 'after-save-hook
+      (lambda
+        (if
+            (yes-or-no-p "Tangle?")
+            (org-babel-tangle)))
+      nil t)
+     (eval add-hook 'after-save-hook
+      (lambda nil
+        (if
+            (yes-or-no-p "Tangle?")
+            (org-babel-tangle)))
+      nil t)
+     (eval add-hook! 'after-save-hook
+      (lambda nil
+        (if
+            (yes-or-no-p "Tangle?")
+            (org-babel-tangle)
+          nil)))
+     (eval add-hook! 'after-save-hook
+      (if
+          (yes-or-no-p "Tangle?")
+          (org-babel-tangle)
+        nil))
+     (evil-shift-width . 2)
      (eval add-hook! 'after-save-hook #'org-babel-tangle)
      (eval add-hook 'after-save-hook
       (lambda nil
@@ -20,4 +43,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
