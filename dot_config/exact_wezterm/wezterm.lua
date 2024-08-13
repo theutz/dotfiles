379e03b1a15dc1af5cfef70d9ee35e5a9ce7d979
Michael Utz <michael@theutz.com>
2024-08-13 20:15:42 +0300

    Update .config/wezterm/wezterm.lua

--- dot_config/exact_wezterm/wezterm.lua
+++ dot_config/exact_wezterm/wezterm.lua
@@ -10,14 +10,14 @@ c:set_strict_mode(true)
 -- c.default_prog = { "zsh", "-c", "/opt/homebrew/bin/nu" }
 c.macos_window_background_blur = 20
 c.font = wezterm.font("BlexMono Nerd Font Mono", { weight = 500 })
-c.font_size = 14
-c.line_height = 1.2
+c.font_size = 16
+c.line_height = 1.6
 c.bold_brightens_ansi_colors = "BrightAndBold"
 c.window_close_confirmation = "NeverPrompt"
 c.quit_when_all_windows_are_closed = true
 c.window_decorations = "RESIZE"
 c.adjust_window_size_when_changing_font_size = false
-local padding = "18px"
+local padding = "16px"
 c.window_padding = {
 	top = padding,
 	bottom = "0",
@@ -30,22 +30,6 @@ c.use_fancy_tab_bar = false
 c.tab_max_width = 80
 c.show_tab_index_in_tab_bar = true
 c.command_palette_font_size = 18
-
-function get_appearance()
-	if wezterm.gui then
-		return wezterm.gui.get_appearance()
-	end
-	return "dark"
-end
-
-function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "rose-pine"
-	else
-		return "rose-pine-dawn"
-	end
-end
-
-c.color_scheme = scheme_for_appearance(get_appearance())
+c.color_scheme = "ChallengerDeep"
 
 return c
