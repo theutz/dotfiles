76512c4637bad4291b6a3903b40547ff68911d72
Michael Utz <michael@theutz.com>
2024-09-10 15:04:23 +0300

    Update home/50-install-rustup.sh

--- home/run_after_50-install-rustup.sh
+++ home/run_after_50-install-rustup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+if ! command -v rustup >/dev/null; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+fi
