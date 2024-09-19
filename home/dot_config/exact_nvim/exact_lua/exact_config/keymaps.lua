4486921364527f075e22268e55398374f7d1ced2
Michael Utz <michael@theutz.com>
2024-09-19 22:39:55 +0300

    Update home/.config/nvim/lua/config/keymaps.lua Update home/.config/nvim/lazy-lock.json

--- home/dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -3,9 +3,20 @@
 -- Add any additional keymaps here
 
 local set = vim.keymap.set
+local del = vim.keymap.del
 
+del("n", "H")
+del("n", "L")
 set("n", "<leader>ck", "<Cmd>LspRestart<cr>", { desc = "Restart LSP" })
+set("n", "<leader>wo", "<Cmd>wincmd o<cr>", { desc = "Close Other Windows" })
+set("n", "<leader>ws", "<cmd>sp<cr>", { desc = "Split horizontal" })
+set("n", "<leader>wv", "<cmd>vsp<cr>", { desc = "Split vertical" })
 set("t", "<S-space>", "<Space>")
-set("n", "<leader>e", function()
-	require("mini.files").open(vim.api.nvim_buf_get_name(0))
-end, { desc = "Open mini.files" })
+set("i", "<C-j>", "<Esc>o<Esc>^Di", { desc = "Create a new blank line below" })
+set("i", "<S-C-j>", "<Esc>O<Esc>^Di", { desc = "Create a new blank line above" })
+set(
+  "n",
+  "<leader>e",
+  function() require("mini.files").open(vim.api.nvim_buf_get_name(0)) end,
+  { desc = "Open mini.files" }
+)
