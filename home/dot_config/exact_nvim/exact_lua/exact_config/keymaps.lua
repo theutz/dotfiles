bbcd97c63b3d08a4b37cb817b0db069ee0193c5c
Michael Utz <michael@theutz.com>
2024-09-19 14:13:10 +0300

    Update home/.config/nvim/lua/config/keymaps.lua

--- home/dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -23,4 +23,9 @@ set("n", "<leader>wv", "<cmd>vsp<cr>", { desc = "Split vertical" })
 set("t", "<S-space>", "<Space>")
 set("i", "<C-j>", "<Esc>o<Esc>^Di", { desc = "Create a new blank line below" })
 set("i", "<S-C-j>", "<Esc>O<Esc>^Di", { desc = "Create a new blank line above" })
-set("n", "<leader>e", function() require("mini.files").open() end, { desc = "Open mini.files" })
+set(
+  "n",
+  "<leader>e",
+  function() require("mini.files").open(vim.api.nvim_buf_get_name(0)) end,
+  { desc = "Open mini.files" }
+)
