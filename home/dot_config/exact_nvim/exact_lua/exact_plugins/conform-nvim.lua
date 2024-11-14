7c89916ae61a1b44f0378ca6524714508e0d92ad
Michael Utz <michael@theutz.com>
2024-11-15 01:07:06 +0300

    Update home/.config/nvim/lua/plugins/conform-nvim.lua Change attributes of home/.zlogin Change attributes of home/.zlogout Change attributes of home/.zpreztorc

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/conform-nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/conform-nvim.lua
@@ -3,12 +3,13 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        lua = { "stylua" },
-        php = { "pint" },
         blade = { "blade-formatter", "rustywind" },
-        javascript = { "prettierd" },
         html = { "prettierd", "rustywind" },
         htmlhugo = { "prettierd", "rustywind" },
+        javascript = { "prettierd" },
+        lua = { "stylua" },
+        php = { "pint" },
+        zsh = { "shfmt" },
       },
     },
   },
