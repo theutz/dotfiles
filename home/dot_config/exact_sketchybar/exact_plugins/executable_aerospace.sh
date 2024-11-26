2ff2093d63e21648da574b18c1f9b976a4985dfa
Michael Utz <michael@theutz.com>
2024-11-26 19:35:54 +0300

    Remove home/.config/sketchybar/plugins/aerospace-switch.sh Add home/.config/sketchybar/plugins/aerospace-switch.sh Update home/.config/sketchybar/plugins/aerospace.sh Update home/.config/sketchybar/sketchybarrc

--- home/dot_config/exact_sketchybar/exact_plugins/executable_aerospace.sh
+++ home/dot_config/exact_sketchybar/exact_plugins/executable_aerospace.sh
@@ -3,7 +3,8 @@
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
-if [ "$NAME" = "space.$(aerospace list-workspaces --focused | sd '^\d+-' '')" ]; then
+echo "$NAME"
+if [ "$NAME" = "space.$(aerospace list-workspaces --focused)" ]; then
   sketchybar --set "$NAME" background.drawing=on
 else
   sketchybar --set "$NAME" background.drawing=off
