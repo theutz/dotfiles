6e36b682b518e104309af6614c02be5537ed3dae
Michael Utz <michael@theutz.com>
2024-11-26 19:12:33 +0300

    Update home/.config/nvim/lua/plugins/neo-tree.lua Update home/.config/nvim/lua/plugins/yazi-nvim.lua Update home/.config/nvim/lazy-lock.json Update home/.config/sketchybar/plugins/aerospace-switch.sh

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/yazi-nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/yazi-nvim.lua
@@ -1,7 +1,7 @@
 return {
   {
     "mikavilpas/yazi.nvim",
-    enabled = true,
+    enabled = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
--- home/dot_config/exact_sketchybar/exact_plugins/executable_aerospace-switch.sh
+++ home/dot_config/exact_sketchybar/exact_plugins/executable_aerospace-switch.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/env zsh
+
+typeset -A workspaces
+
+{{ range $i, $v .workspaces }}
+workspaces[{{ $v.name }}]=
+{{ end }}
+workspaces=(
+
+)
 workspace="$(echo "$NAME" | cut -d. -f2)"
 aerospace workspace "$workspace"
