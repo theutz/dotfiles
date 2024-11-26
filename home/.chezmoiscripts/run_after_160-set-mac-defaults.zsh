3bee949c438aef0e908b4d4bc68d73c44b936286
Michael Utz <michael@theutz.com>
2024-11-26 14:32:15 +0300

    Update home/.chezmoiscripts/160-set-mac-defaults.zsh Update home/.config/aerospace/aerospace.toml

--- home/.chezmoiscripts/run_after_160-set-mac-defaults.zsh
+++ home/.chezmoiscripts/run_after_160-set-mac-defaults.zsh
@@ -7,3 +7,9 @@ defaults write -g NSWindowShouldDragOnGesture -bool true
 
 # https://nikitabobko.github.io/AeroSpace/goodies#disable-open-animations
 defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+
+# https://nikitabobko.github.io/AeroSpace/guide#a-note-on-mission-control
+defaults write com.apple.dock expose-group-apps -bool true && killall Dock
+
+# https://nikitabobko.github.io/AeroSpace/guide#a-note-on-displays-have-separate-spaces
+defaults write com.apple.spaces spans-displays -bool true && killall SystemUIServer
