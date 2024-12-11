9c9cb90a74dcf6f558d8b10a7ffdd70ab3e44a30
Michael Utz <michael@theutz.com>
2024-12-11 12:36:42 +0300

    Update home/.config/nvim/lua/plugins/conform-nvim.lua

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/conform-nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/conform-nvim.lua
@@ -2,11 +2,6 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      formatters = {
-        rubocop = {
-          args = { "--server", "--auto-correct-all", "--stderr", "--force-exclusion", "--stdin", "$FILENAME" },
-        },
-      },
       formatters_by_ft = {
         blade = { "blade-formatter", "rustywind" },
         html = { "prettierd", "rustywind" },
