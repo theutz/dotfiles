afe62c1626df8f5ea8486bd315cf91505d7115c9
Michael Utz <michael@theutz.com>
2024-12-13 12:16:14 +0300

    Update home/.config/mise/config.toml Add home/.config/nvim/lua/plugins/overseer-nvim.lua Update home/.config/nvim/lazy-lock.json

---
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/overseer-nvim.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "stevearc/overseer.nvim",
+    keys = {
+      { "<leader>ol", "<Cmd>OverseerLoadBundle<cr>", desc = "Load bundle" },
+      { "<leader>os", "<Cmd>OverseerSaveBundle<cr>", desc = "Save bundle" },
+    },
+  },
+}
