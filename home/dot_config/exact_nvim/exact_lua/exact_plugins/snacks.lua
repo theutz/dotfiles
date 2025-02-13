d8bc66409f78a55617dc6e53a7decd7a7454542e
Michael Utz <michael@theutz.com>
2025-02-13 20:23:20 +0300

    Update home/.config/btop/btop.conf Update home/.config/linode-cli Add home/.config/nvim/lua/plugins/snacks.lua Update home/.config/nvim/lazy-lock.json

---
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    -- explorer = {
+    --   replace_netrw = true,
+    -- },
+    picker = {
+      sources = {
+        files = {
+          hidden = true,
+        },
+        explorer = {
+          hidden = true,
+        },
+      },
+    },
+  },
+}
