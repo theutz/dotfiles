ed4cf285428f46146a61fde55fc77e062fd5dc2a
Michael Utz <michael@theutz.com>
2025-02-18 15:10:55 +0300

    Update home/.config/nvim/lua/plugins/winshift.lua Add home/.ssh/config.d/friendzy.delegator.com

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/winshift.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/winshift.lua
@@ -5,6 +5,10 @@ return {
     keys = {
       { "<leader>wm", "<Cmd>WinShift<cr>", desc = "Move window" },
       { "<leader>ww", "<Cmd>WinShift swap<cr>", desc = "Swap window" },
+      { "<C-M-h>", "<Cmd>WinShift left<cr>", desc = "Shift window left" },
+      { "<C-M-j>", "<Cmd>WinShift down<cr>", desc = "Shift window down" },
+      { "<C-M-k>", "<Cmd>WinShift up<cr>", desc = "Shift window up" },
+      { "<C-M-l>", "<Cmd>WinShift right<cr>", desc = "Shift window right" },
     },
   },
 }
