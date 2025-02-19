fd90de0398f4f65eb0bd2d8c566228e4caf3d893
Michael Utz <michael@theutz.com>
2025-02-19 15:31:31 +0300

    Update home/.config/btop/btop.conf Update home/.config/wezterm/wezterm.lua Update home/.config/prezto/modules/aliases/init.zsh Update home/.zpreztorc

--- home/dot_config/prezto/modules/aliases/init.zsh
+++ home/dot_config/prezto/modules/aliases/init.zsh
@@ -9,3 +9,7 @@ alias ls='eza --group --group-directories-first --icons'
 alias reload='exec zsh'
 alias rl='exec zsh'
 alias aliases="\${(z)EDITOR} ${0:a}"
+
+# Wezterm
+alias wez='wezterm cli'
+alias sshw='wezterm ssh'
