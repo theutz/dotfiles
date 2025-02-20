e6dc4a001294df6ce7fec91ccfe769dde031c68a
Michael Utz <michael@theutz.com>
2025-02-20 20:02:42 +0300

    Update home/.config/nvim/lua/plugins/colorschemes.lua Update home/.config/nvim/lazy-lock.json Update home/.config/tmux/tmux.conf Update home/.config/wezterm/wezterm.lua

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/colorschemes.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/colorschemes.lua
@@ -1,108 +1,124 @@
 local colors = {
-	{
-		"folke/tokyonight.nvim",
-		enabled = false,
-		opts = {
-			transparent = true,
-			style = "moon",
-			light_style = "moon",
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-			on_colors = function(c)
-				c.border = c.blue0
-			end,
-		},
-	},
-	{
-		"scottmckendry/cyberdream.nvim",
-		enabled = false,
-		opts = {
-			transparent = true,
-			italic_comments = true,
-		},
-	},
-	{
-		"echasnovski/mini.base16",
-		enabled = false,
-		version = false,
-	},
-	{
-		"uloco/bluloco.nvim",
-		enabled = false,
-		dependencies = { "rktjmp/lush.nvim" },
-		opts = {
-			transparent = true,
-			style = "auto",
-			italics = true,
-		},
-	},
-	{
-		"bluz71/vim-moonfly-colors",
-		enabled = false,
-		name = "moonfly",
-	},
-	{
-		"nyoom-engineering/oxocarbon.nvim",
-		enabled = false,
-	},
-	{
-		"austinliuigi/smoke.nvim",
-		enabled = false,
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		enabled = false,
-		opts = {
-			transparent_mode = true,
-		},
-	},
-	{
-		"shaunsingh/nord.nvim",
-		enabled = false,
-		init = function()
-			vim.g.nord_disable_background = true
-		end,
-	},
-	{
-		"rose-pine/neovim",
-		enabled = false,
-		name = "rose-pine",
-		opts = {
-			variant = "dawn",
-			dark_variant = "main",
-			styles = {
-				transparency = true,
-			},
-		},
-	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		opts = {
-			transparent_background = true,
-			background = {
-				light = "latte",
-				dark = "mocha",
-			},
-		},
-	},
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    enabled = false,
+    opts = {
+      transparent = true,
+      style = "moon",
+      light_style = "moon",
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_colors = function(c) c.border = c.blue0 end,
+    },
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    enabled = false,
+    opts = {
+      transparent = true,
+      italic_comments = true,
+    },
+  },
+  {
+    "echasnovski/mini.base16",
+    enabled = false,
+    version = false,
+  },
+  {
+    "uloco/bluloco.nvim",
+    enabled = false,
+    dependencies = { "rktjmp/lush.nvim" },
+    opts = {
+      transparent = true,
+      style = "auto",
+      italics = true,
+    },
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    enabled = false,
+    name = "moonfly",
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    enabled = false,
+  },
+  {
+    "austinliuigi/smoke.nvim",
+    enabled = false,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    enabled = false,
+    opts = {
+      transparent_mode = true,
+    },
+  },
+  {
+    "shaunsingh/nord.nvim",
+    enabled = false,
+    init = function() vim.g.nord_disable_background = true end,
+  },
+  {
+    "rose-pine/neovim",
+    enabled = false,
+    name = "rose-pine",
+    opts = {
+      variant = "dawn",
+      dark_variant = "main",
+      styles = {
+        transparency = true,
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    enabled = true,
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+    },
+  },
 }
 
+local function get_enabled_scheme_name()
+  local enabled = {}
+
+  for _, plugin in ipairs(colors) do
+    if plugin.enabled then
+      local _, repo = plugin[1]:match "([^/]+)/([^/]+)"
+      local name = plugin.name or repo
+      table.insert(enabled, name)
+    end
+  end
+
+  if #enabled > 1 then
+    return "tokyonight"
+  else
+    return enabled[1]
+  end
+end
+
 local plugins = {
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "catppuccin",
-		},
-	},
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = get_enabled_scheme_name(),
+    },
+  },
 }
 
 for _, color in pairs(colors) do
-	color.priority = 1000
-	color.lazy = false
-	table.insert(plugins, color)
+  color.priority = 1000
+  color.lazy = false
+  table.insert(plugins, color)
 end
 
 return plugins
