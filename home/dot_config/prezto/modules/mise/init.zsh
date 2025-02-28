276f0c83633c81c80d4ab8994ffec8f141c21d3f
Michael Utz <michael@theutz.com>
2025-02-28 11:33:54 +0300

    Update home/.config/mise/config.toml Update home/.config/prezto/modules/mise/init.zsh

--- home/dot_config/prezto/modules/mise/init.zsh
+++ home/dot_config/prezto/modules/mise/init.zsh
@@ -4,9 +4,17 @@ eval "$(~/.local/bin/mise activate zsh)"
 # aliases
 
 alias m='mise'
-alias mr='mise run --'
+alias me='mise --env'
+
+alias mr='mise run'
 alias mt='mise tasks --extended'
+alias mte='mise tasks edit'
+alias mta='mise tasks add'
+
 alias mu='mise use'
 alias mug='mise use --global'
-alias me='mise --env'
+alias mup='mise use --pin'
+alias mupg='mise use --pin --global'
+alias mugp='mise use --pin --global'
+
 alias mx='mise exec'
