d9b00c3f2ff990690d33728c4f34a20556890ec6
Michael Utz <michael@theutz.com>
2025-03-06 20:05:43 +0300

    Update home/.config/nvim/lua/config/keymaps.lua Update home/.config/tmux/command-aliases.conf

--- home/dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -9,6 +9,10 @@ local set = vim.keymap.set
 set("t", "<S-space>", "<Space>")
 -- set("i", "<C-j>", "<Esc>o<Esc>^Di", { desc = "Create a new blank line below" })
 -- set("i", "<S-C-j>", "<Esc>O<Esc>^Di", { desc = "Create a new blank line above" })
--- set("n", "[<tab>", "<cmd>tabprevious<cr>", { desc = "Previous tab" })
--- set("n", "]<tab>", "<cmd>tabnext<cr>", { desc = "Next tab" })
 -- set("n", "gJ", "Jx", { desc = "Join with next line (no space)" })
+
+set("n", "[<tab>", "<cmd>tabprevious<cr>", { desc = "Previous tab" })
+set("n", "<leader><tab>p", "<cmd>tabprevious<cr>", { desc = "Previous tab" })
+
+set("n", "]<tab>", "<cmd>tabnext<cr>", { desc = "Next tab" })
+set("n", "<leader><tab>n", "<cmd>tabnext<cr>", { desc = "Next tab" })
