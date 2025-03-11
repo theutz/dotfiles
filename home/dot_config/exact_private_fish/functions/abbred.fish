bce9828dc4791edd0eaa529bf45ab7744381efe8
Michael Utz <michael@theutz.com>
2025-03-11 21:15:09 +0300

    Add home/.config/fish/functions/abbred.fish Add home/.config/fish/functions/cmfzf.fish Add home/.config/fish/functions/fished.fish Change attributes of home/.config/fish/functions/fish_user_key_bindings.fish Change attributes of home/.config/fish/functions/ls.fish Change attributes of home/.config/fish/functions/br.fish Change attributes of home/.config/fish/functions/fzf_key_bindings.fish

---
+++ home/dot_config/exact_private_fish/functions/abbred.fish
@@ -0,0 +1,3 @@
+function abbred --description 'Edit abbreviations'
+    chezmoi edit --watch $fish_abbr_file
+end
---
+++ home/dot_config/exact_private_fish/functions/fished.fish
@@ -0,0 +1,3 @@
+function fished --description 'Edit fish config'
+    chezmoi edit --watch ~/.config/fish
+end
