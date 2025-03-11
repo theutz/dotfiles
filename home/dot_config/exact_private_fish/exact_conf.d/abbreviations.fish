a7adf7530f06d0cae6d73a555f6e0b12bdf16fdb
Michael Utz <michael@theutz.com>
2025-03-11 12:53:23 +0300

    Update home/.config/fish/conf.d/abbreviations.fish

--- home/dot_config/exact_private_fish/exact_conf.d/abbreviations.fish
+++ home/dot_config/exact_private_fish/exact_conf.d/abbreviations.fish
@@ -2,3 +2,5 @@ abbr -a -- gco 'git checkout'
 abbr -a -- c chezmoi
 abbr -a -- cms 'chezmoi status'
 abbr -a -- cmsw 'chezmoi status-watch'
+abbr -a -- cmd 'chezmoi diff'
+abbr -a -- cmra 'chezmoi re-add'
