3ffb96d3252eefd6898b83f5f74b1d9b8c02b916
Michael Utz <michael@theutz.com>
2025-03-11 10:08:46 +0300

    Update home/.config/nushell/config.nu Update home/.config/kitty/open-actions.conf Update home/.config/kitty/startup.sh Update home/.zprofile

--- home/dot_config/kitty/startup.sh
+++ home/dot_config/kitty/startup.sh
@@ -1,15 +1,1 @@
-cd ~
-title home
-launch zsh
 
-new_tab delegator
-cd ~/Sites/delegator
-layout tall:bias=70
-launch zsh
-launch zsh
-
-new_tab funding-tool
-cd ~/Sites/funding-tool
-layout tall:bias=70
-launch zsh
-launch zsh
