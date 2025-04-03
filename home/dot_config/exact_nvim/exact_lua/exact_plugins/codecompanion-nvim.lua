74f84e464b5247f07ad80f874d7d654b933f4bef
Michael Utz <michael@theutz.com>
2025-04-03 12:26:23 +0300

    Update home/.config/nvim/lua/plugins/codecompanion-nvim.lua

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/codecompanion-nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/codecompanion-nvim.lua
@@ -1,6 +1,18 @@
 return {
   "olimorris/codecompanion.nvim",
-  config = true,
+  config = {
+    strategies = {
+      chat = {
+        adapter = "anthropic",
+      },
+      inline = {
+        adapter = "anthropic",
+      },
+      cmd = {
+        adapter = "anthropic",
+      },
+    },
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
