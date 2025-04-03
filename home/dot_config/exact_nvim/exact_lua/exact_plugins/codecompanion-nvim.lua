4f80af02e98afd71959e8c91764cc4512b06a156
Michael Utz <michael@theutz.com>
2025-04-03 13:00:18 +0300

    Update home/.config/nvim/lua/plugins/codecompanion-nvim.lua

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/codecompanion-nvim.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/codecompanion-nvim.lua
@@ -1,27 +1,27 @@
 return {
-  "olimorris/codecompanion.nvim",
-  init = {
-    require("which-key").add { "<leader>a", group = "ai" },
-  },
-  config = {
-    strategies = {
-      chat = {
-        adapter = "anthropic",
-      },
-      inline = {
-        adapter = "anthropic",
-      },
-      cmd = {
-        adapter = "anthropic",
-      },
-    },
-  },
-  keys = {
-    { "<leader>ac", "<Cmd>CodeCompanionChat<CR>", desc = "Chat (Code Companion)" },
-    { "<leader>aa", "<Cmd>CodeCompanionActions<CR>", desc = "Actions (Code Companion)" },
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
+	"olimorris/codecompanion.nvim",
+	config = function()
+		require("which-key").add({ "<leader>a", group = "ai" })
+		return {
+			strategies = {
+				chat = {
+					adapter = "anthropic",
+				},
+				inline = {
+					adapter = "anthropic",
+				},
+				cmd = {
+					adapter = "anthropic",
+				},
+			},
+		}
+	end,
+	keys = {
+		{ "<leader>ac", "<Cmd>CodeCompanionChat<CR>", desc = "Chat (Code Companion)" },
+		{ "<leader>aa", "<Cmd>CodeCompanionActions<CR>", desc = "Actions (Code Companion)" },
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
 }
