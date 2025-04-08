ef24f5cf7ed5be9c44e75b70a84777fce0d4df5c
Michael Utz <michael@theutz.com>
2025-04-08 23:05:22 +0300

    Update home/.chezmoiexternal.toml Remove home/.config/fish/completions/.keep Remove home/.config/fish/config.fish Remove home/.config/fish/conf.d/.keep Remove home/.config/fish/conf.d/abbreviations.fish Remove home/.config/fish/conf.d/event-handlers.fish Remove home/.config/fish/conf.d/ls-colors.fish Remove home/.config/fish/conf.d/mise.fish Remove home/.config/fish/conf.d/zoxide.fish Remove home/.config/fish/functions/abbred.fish Remove home/.config/fish/functions/cmfzf.fish Remove home/.config/fish/functions/daylight.fish Remove home/.config/fish/functions/edit.fish Remove home/.config/fish/functions/fish_user_key_bindings.fish Remove home/.config/fish/functions/fished.fish Remove home/.config/fish/functions/ls.fish Remove home/.config/fish/functions/br.fish Remove home/.config/fish/functions/fzf_key_bindings.fish Remove home/.config/fish/functions/weather.fish Remove home/.config/fish/functions/y.fish Remove home/.config/fish/fish_variables

--- home/dot_config/exact_private_fish/exact_conf.d/event-handlers.fish
+++
@@ -1,6 +0,0 @@
-function readd_abbr --on-event fish_postexec
-    if string match -rq "^abbr (-a|--add)\b" $argv[1]
-        abbr >$fish_abbr_file
-        chezmoi re-add $fish_abbr_file
-    end
-end
--- home/dot_config/exact_private_fish/exact_functions/cmfzf.fish
+++
@@ -1,7 +0,0 @@
-function cmfzf --description 'Use fzf to filter files in chezmoi status'
-    set files $(chezmoi status | fzf --multi | awk '{ print $2 }')
-
-    for file in $files
-        string join / $HOME $file
-    end
-end
