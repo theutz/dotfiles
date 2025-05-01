fb5ba22d42738841c2e9217fe70d2e5e7fdde63d
Michael Utz <michael@theutz.com>
2025-05-01 19:39:37 +0300

    Update home/.config/ghostty/config Update home/.config/mise/config.toml Update home/.hammerspoon/config/spoons/recursive-binder.lua Update home/.hammerspoon/init.lua

--- home/exact_dot_hammerspoon/exact_config/exact_spoons/recursive-binder.lua
+++ home/exact_dot_hammerspoon/exact_config/exact_spoons/recursive-binder.lua
@@ -5,35 +5,62 @@ s:andUse "RecursiveBinder"
 
 local bind = spoon.RecursiveBinder.recursiveBind
 
-local open_in_wezterm = function(command)
-	local args = { "-e", command }
-
+local function wezterm_start(command)
+	local args = { "-e" }
+	if command then
+		table.insert(args, command)
+	end
 	return hs.task.new("/opt/homebrew/bin/wezterm", nil, args):start()
+end
+
+local function wezterm()
+	hs.application.open "WezTerm"
+end
+
+local function spotify_player()
+	local function window()
+		return hs.window.find "spotify_player"
+	end
+
+	if window() then
+		window():focus()
+		return
+	end
+
+	if wezterm_start "/opt/homebrew/bin/spotify_player" then
+		waitUntil(window, function()
+			local w = window()
+			w:focus()
+			hs.grid.set(w, { 1, 1, 10, 10 }, "Built%-in")
+		end):start()
+	end
+end
+
+local function ticker()
+	local function window()
+		return hs.window.find "tickrs"
+	end
+
+	if window() then
+		window():focus()
+		return
+	end
+
+	if wezterm_start "/opt/homebrew/bin/tickrs" then
+		waitUntil(window, function()
+			local w = window()
+			w:focus()
+			hs.grid.set(w, { 1, 1, 10, 10 }, "Built%-in")
+		end)
+	end
 end
 
 hs.hotkey.bind(
 	nil,
 	"f13",
 	bind {
-		[{ {}, "s", "spotify" }] = function()
-			local get_win = function()
-				return hs.window.find "spotify_player"
-			end
-
-			if get_win() then
-				get_win():focus()
-				return
-			end
-
-			if open_in_wezterm "/opt/homebrew/bin/spotify_player" then
-				waitUntil(get_win, function()
-					local w = get_win()
-					hs.grid.set(w, { 1, 1, 10, 10 })
-					w:focus()
-				end):start()
-			end
-		end,
+		[{ {}, "w", "wezterm" }] = wezterm,
+		[{ {}, "s", "spotify player" }] = spotify_player,
+		[{ {}, "t", "ticker" }] = ticker,
 	}
 )
-
-return bind
--- home/exact_dot_hammerspoon/init.lua
+++ home/exact_dot_hammerspoon/init.lua
@@ -2,7 +2,19 @@
 require "hs.ipc"
 
 -- Load the local configuration files
-local config = require "config"
-config:load()
+-- package.path = package.path .. ";" .. hs.configdir .. "/?.spoon/init.lua"
+
+---@type spoon.SpoonInstall
+local s = assert(hs.loadSpoon "SpoonInstall", "Error while loading SpoonInstall")
+
+local mash = { "ctrl", "cmd", "alt" }
+local hyper = { "shift", table.unpack(mash) }
+
+spoon.SpoonInstall:andUse("ReloadConfiguration", {
+	hotkeys = {
+		reloadConfiguration = { mash, "r" },
+	},
+	start = true,
+})
 
 hs.notify.show("Hammerspoon", "", "Config reloaded!")
