e248b460493ecccbad3490acb0d87c4faccda7c4
Michael Utz <michael@theutz.com>
2025-05-01 10:56:44 +0300

    Add home/.config/nvim/lua/plugins/yazi-nvim.lua

---
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/yazi-nvim.lua
@@ -0,0 +1,17 @@
+return {
+	"mikavilpas/yazi.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"folke/snacks.nvim",
+	},
+	keys = {
+		{ "<leader>e", "<cmd>Yazi<cr>", desc = "Open file explorer (current file)" },
+		{ "<leader>E", "<cmd>Yazi cwd<cr>", desc = "Open file explorer (cwd)" },
+	},
+	opts = {
+		open_for_directories = true,
+	},
+	init = function()
+		vim.g.loaded_netrwPlugin = 1
+	end,
+}
