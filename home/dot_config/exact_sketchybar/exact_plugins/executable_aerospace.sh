a01fdedc3d5c4a202f71c6d82e8d43d770000fec
Michael Utz <michael@theutz.com>
2025-05-03 01:19:32 +0300

    Update home/.config/aerospace/aerospace.toml Update home/.config/sketchybar/plugins/aerospace.sh Update home/.config/sketchybar/sketchybarrc

--- home/dot_config/exact_sketchybar/exact_plugins/executable_aerospace.sh
+++ home/dot_config/exact_sketchybar/exact_plugins/executable_aerospace.sh
@@ -11,58 +11,37 @@ workspaces_on_focused_monitor="$(
 )"
 
 function is_on_focused_monitor() {
-    echo "$workspaces_on_focused_monitor" |
-        jq --exit-status --arg w "$1" 'any(. == $w)' >/dev/null
+    jq --exit-status --arg w "$1" 'any(. == $w)' >/dev/null <<<"$workspaces_on_focused_monitor"
     return $?
 }
 
 function workspace_has_windows() {
     count="$(aerospace list-windows --workspace "$1" --count)"
-    if [[ "$count" -gt 0 ]]; then
-        return 0
-    else
-        return 1
-    fi
+    test "$count" -gt 0
+    return $?
 }
 
 args=()
 
 for workspace in $all_workspaces; do
-    args+=(
-        --set
-        space."$workspace"
-    )
+    args+=(--set space."$workspace")
 
     if [[ "$workspace" == "$focused_workspace" ]]; then
-        args+=(
-            background.drawing=on
-            # label.highlight=on
-            # icon.highlight=on
-        )
+        args+=(background.drawing=on)
     else
-        args+=(
-            background.drawing=off
-        )
+        args+=(background.drawing=off)
     fi
 
     if is_on_focused_monitor "$workspace"; then
-        args+=(
-            label.highlight=on
-        )
+        args+=(label.highlight=on)
     else
-        args+=(
-            label.highlight=off
-        )
+        args+=(label.highlight=off)
     fi
 
     if workspace_has_windows "$workspace"; then
-        args+=(
-            icon.highlight=on
-        )
+        args+=(icon.highlight=on)
     else
-        args+=(
-            icon.highlight=off
-        )
+        args+=(icon.highlight=off)
     fi
 done
 
