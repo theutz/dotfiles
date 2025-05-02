7650e891880f73c99a55b32742e3392fb1b163a5
Michael Utz <michael@theutz.com>
2025-05-02 11:14:37 +0300

    Update home/.config/aerospace/aerospace.toml Update home/.config/aerospace/.taplo.toml Update home/.config/nvim/lua/config/keymaps.lua

--- home/dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -5,7 +5,7 @@
 local set = vim.keymap.set
 -- local del = vim.keymap.del
 
--- set("n", "<leader>ck", "<Cmd>LspRestart<cr>", { desc = "Restart LSP" })
+set("n", "<leader>ck", "<Cmd>LspRestart<cr>", { desc = "Restart LSP" })
 set("t", "<S-space>", "<Space>")
 -- set("i", "<C-j>", "<Esc>o<Esc>^Di", { desc = "Create a new blank line below" })
 -- set("i", "<S-C-j>", "<Esc>O<Esc>^Di", { desc = "Create a new blank line above" })
