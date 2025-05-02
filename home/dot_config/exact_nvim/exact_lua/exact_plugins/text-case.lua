2691cfa1dd83659d38fb7d7e3ccf689b9786a837
Michael Utz <michael@theutz.com>
2025-05-02 10:25:09 +0300

    Update home/.config/aerospace/aerospace.toml Update home/.config/mise/config.toml Update home/.config/nvim/lua/plugins/text-case.lua Update home/.config/nvim/lazy-lock.json Update home/.config/wezterm/config/gui.lua

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/text-case.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/text-case.lua
@@ -1,21 +1,23 @@
 return {
-  {
-    "johmsalas/text-case.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      require("textcase").setup {}
-      require("telescope").load_extension "textcase"
-    end,
-    keys = {
-      {
-        "ga.",
-        "<cmd>TextCaseOpenTelescope<cr>",
-        mode = { "n", "v" },
-        desc = "Telescope",
-      },
-    },
-  },
+	{
+		"johmsalas/text-case.nvim",
+		enabled = false,
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			require("textcase").setup({})
+			require("telescope").load_extension("textcase")
+		end,
+		keys = {
+			"<leader>c.",
+			{
+				"<leader>c.",
+				"<cmd>TextCaseOpenTelescope<cr>",
+				mode = { "n", "v" },
+				desc = "Telescope",
+			},
+		},
+	},
 }
