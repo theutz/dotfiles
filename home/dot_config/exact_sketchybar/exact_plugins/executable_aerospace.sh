d2c5ef86875474f7ebc15e4d43246bdf5cbe524b
Michael Utz <michael@theutz.com>
2025-05-02 17:25:23 +0300

    Update home/.config/aerospace/aerospace.toml Update home/.config/sketchybar/plugins/aerospace.sh Update home/.config/sketchybar/sketchybarrc

--- home/dot_config/exact_sketchybar/exact_plugins/executable_aerospace.sh
+++ home/dot_config/exact_sketchybar/exact_plugins/executable_aerospace.sh
@@ -6,15 +6,34 @@ if [[ -z "$FOCUSED_WORKSPACE" ]]; then
     FOCUSED_WORKSPACE="$(aerospace list-workspaces --focused)"
 fi
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME \
-        background.drawing=on \
-        label.color="$(dracula -s background)" \
-        label.padding_left=8 \
-        label.padding_right=8
+args=("--set" "$NAME")
+
+if [[ $(aerospace list-workspaces --monitor focused) =~ $1 ]]; then
+    args+=(icon="•")
 else
-    sketchybar --set $NAME background.drawing=off \
-        label.color="$(dracula -s foreground)" \
-        label.padding_left=0 \
-        label.padding_right=0
+    args+=(icon=" ")
 fi
+
+if [[ "$1" = "$FOCUSED_WORKSPACE" ]]; then
+    args+=(
+        background.drawing=on
+        icon.drawing=off
+        label.color="$(dracula -s background)"
+    )
+else
+    args+=(
+        background.drawing=off
+        icon.drawing=on
+    )
+    if [[ "$(aerospace list-windows --workspace "$1" --count)" -gt 0 ]]; then
+        args+=(
+            label.color="$(dracula -s foreground)"
+        )
+    else
+        args+=(
+            label.color="$(dracula -s comment)"
+        )
+    fi
+fi
+
+sketchybar "${args[@]}"
