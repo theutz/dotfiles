9710e35d19b6b668c1941ea4760d69d12fdd3f67
Michael Utz <michael@theutz.com>
2025-05-02 11:48:02 +0300

    Update home/.config/aerospace/aerospace.toml Update home/.config/fish/config.fish Update home/.config/nvim/lua/plugins/avante-nvim.lua Update home/.config/nvim/lazy-lock.json

--- home/dot_config/exact_fish/config.fish
+++ home/dot_config/exact_fish/config.fish
@@ -1,2 +1,12 @@
-# Initialize starship
-starship init fish | source
+$HOME/.local/bin/mise activate fish | source
+
+if status is-interactive
+    # Initialize starship
+    starship init fish | source
+
+    # Setup atuin
+    atuin init fish | source
+
+    # Setup zoxide
+    zoxide init fish | source
+end
