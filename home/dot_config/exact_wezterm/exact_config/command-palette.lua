85cfb2217535f7461f39f7eb0581853fafb4da14
Michael Utz <michael@theutz.com>
2025-05-06 22:37:57 +0300

    Update home/.config/wezterm/config/command-palette.lua

--- home/dot_config/exact_wezterm/exact_config/command-palette.lua
+++ home/dot_config/exact_wezterm/exact_config/command-palette.lua
@@ -19,6 +19,20 @@ function M.augment_command_palette(window, pane)
 end
 
 function M.apply_to_config(config)
+	-- ISSUE: Waiting for this to come to main https://wezterm.org/config/lua/config/command_palette_font.html
+	-- if config.font then config.command_palette_font = config.font end
+
+	local name = config.color_scheme
+	local scheme = wezterm.color.get_builtin_schemes()[name]
+	local fg = wezterm.color.parse(scheme.ansi[5])
+	local bg = wezterm.color.parse(scheme.background)
+
+	config.ui_key_cap_rendering = "UnixLong"
+	config.command_palette_fg_color = fg:lighten(0.1)
+	config.command_palette_bg_color = bg:lighten(0.1)
+	config.command_palette_font_size = 14
+	config.command_palette_rows = 10
+
 	wezterm.on("augment-command-palette", M.augment_command_palette)
 end
 
