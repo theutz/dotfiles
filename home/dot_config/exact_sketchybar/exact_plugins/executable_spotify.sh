4717628f7bfdc0108ff887f4b68188c0d1e181bd
Michael Utz <michael@theutz.com>
2025-05-06 19:36:35 +0300

    Update home/.config/sketchybar/plugins/spotify.sh Update home/.config/sketchybar/sketchybarrc

--- home/dot_config/exact_sketchybar/exact_plugins/executable_spotify.sh
+++ home/dot_config/exact_sketchybar/exact_plugins/executable_spotify.sh
@@ -16,22 +16,10 @@ else
     artist=""
 fi
 
-is_multi="$(sketchybar --query displays | jq -r 'length | . > 1')"
-
 args=(
     --animate quadratic 30
     --set "$NAME"
 )
-
-if $is_multi; then
-    args+=(
-        label.max_chars=0
-    )
-else
-    args+=(
-        label.max_chars=40
-    )
-fi
 
 if $is_playing; then
     args+=(
