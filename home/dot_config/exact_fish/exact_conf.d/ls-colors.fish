5adbd00ac8972661497de08b3f3b4d2a7bcb75d3
Michael Utz <michael@theutz.com>
2025-05-07 16:21:01 +0300

    Update home/.config/fish/conf.d/ls-colors.fish Update home/.config/fish/fish_variables

--- home/dot_config/exact_fish/exact_conf.d/ls-colors.fish
+++ home/dot_config/exact_fish/exact_conf.d/ls-colors.fish
@@ -1,7 +1,1 @@
-if test $(dark-mode status) = on
-    set -g theme rose-pine
-else
-    set -g theme rose-pine-dawn
-end
-set -gx LS_COLORS $(~/.local/share/mise/shims/vivid generate $theme)
-set -e theme
+set -gx LS_COLORS $(~/.local/share/mise/shims/vivid generate dracula)
