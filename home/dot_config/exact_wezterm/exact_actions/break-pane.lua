f3d91d2747ef608e546db50ecfe67838c956ce55
Michael Utz <michael@theutz.com>
2025-05-13 22:03:05 +0300

    Remove home/.config/wezterm/actions/break-pane.lua Remove home/.config/wezterm/actions/rename-workspace.lua Remove home/.config/wezterm/config/colors.lua Remove home/.config/wezterm/config/command-palette.lua Remove home/.config/wezterm/config/gui.lua Remove home/.config/wezterm/config/keys.lua Remove home/.config/wezterm/config/launch-menu.lua Remove home/.config/wezterm/config/mux.lua Remove home/.config/wezterm/config/plugins.lua Remove home/.config/wezterm/config/shell.lua Remove home/.config/wezterm/config/tab-bar.lua Remove home/.config/wezterm/config/workspaces.lua Remove home/.config/wezterm/util/color.lua

--- home/dot_config/exact_wezterm/exact_actions/break-pane.lua
+++
@@ -1,6 +0,0 @@
-local w = require "wezterm"
-
-return w.action_callback(function(_, pane)
-	pane:move_to_new_tab()
-	pane:activate()
-end)
--- home/dot_config/exact_wezterm/exact_config/workspaces.lua
+++
@@ -1,5 +0,0 @@
-local M = {}
-
-function M.apply_to_config(config) config.default_workspace = "home" end
-
-return M
