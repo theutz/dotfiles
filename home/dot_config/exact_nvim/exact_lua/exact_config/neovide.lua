32d3a70e8caca69a607ede2eb737c6b13d1409ba
Michael Utz <michael@theutz.com>
2025-05-13 12:15:34 +0300

    Update home/.config/nvim/lua/config/neovide.lua

--- home/dot_config/exact_nvim/exact_lua/exact_config/neovide.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_config/neovide.lua
@@ -1,19 +1,17 @@
 vim.o.linespace = 16
 vim.o.guifont = "Berkeley Mono:h" .. vim.o.linespace
 
-local padding = 20
+local padding = 8
 vim.g.neovide_padding_top = padding
 vim.g.neovide_padding_bottom = padding
 vim.g.neovide_padding_left = padding
 vim.g.neovide_padding_right = padding
 
-local opacity = 0.85
-vim.g.neovide_opacity = 1.0
-vim.g.neovide_normal_opacity = opacity
+vim.g.neovide_theme = "dark"
+
+vim.g.neovide_opacity = 0.85
+vim.g.neovide_normal_opacity = 0.1
 
 vim.g.neovide_window_blurred = true
 vim.g.neovide_input_macos_option_key_is_meta = "both"
-vim.g.neovide_cursor_animation_length = 0.03
-vim.g.neovide_cursor_trail_size = 0.85
-vim.g.neovide_cursor_vfx_mode = "railgun"
-vim.g.neovide_theme = "auto"
+vim.g.neovide_cursor_vfx_mode = { "pixiedust", "sonicboom" }
