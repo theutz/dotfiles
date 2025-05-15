a7539a23236366083ab20bdec90e3bfe6b36d2d8
Michael Utz <michael@theutz.com>
2025-05-15 12:51:10 +0300

    Update home/.config/nvim/after/queries/toml/injections.scm Update home/.config/nvim/lua/plugins/treesitter.lua

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
@@ -1,56 +1,52 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    init = function()
-      require("vim.treesitter.query").add_predicate(
-        "is-mise?",
-        function(_, _, bufnr, _)
-          local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
-          local filename = vim.fn.fnamemodify(filepath, ":t")
-          return string.match(filename, ".*mise.*%.toml$")
-        end,
-        { force = true, all = false }
-      )
-    end,
-    opts = {
-      ensure_installed = {
-        "awk",
-        "bash",
-        "c",
-        "css",
-        "diff",
-        "dockerfile",
-        "go",
-        "gomod",
-        "gosum",
-        "gowork",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "php",
-        "printf",
-        "python",
-        "query",
-        "regex",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-      },
-      indent = {
-        enable = true,
-        disable = { "yaml", "php" },
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		init = function()
+			require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+				local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+				local filename = vim.fn.fnamemodify(filepath, ":t")
+				return string.match(filename, ".*mise.*%.toml$") ~= nil
+			end, { force = true, all = false })
+		end,
+		opts = {
+			ensure_installed = {
+				"awk",
+				"bash",
+				"c",
+				"css",
+				"diff",
+				"dockerfile",
+				"go",
+				"gomod",
+				"gosum",
+				"gowork",
+				"html",
+				"javascript",
+				"jsdoc",
+				"json",
+				"jsonc",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"php",
+				"printf",
+				"python",
+				"query",
+				"regex",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"xml",
+				"yaml",
+			},
+			indent = {
+				enable = true,
+				disable = { "yaml", "php" },
+			},
+		},
+	},
 }
