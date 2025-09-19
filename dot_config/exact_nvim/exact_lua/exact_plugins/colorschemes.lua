9c466954934949bdad06115f599ba4619ee9fd0b
Michael Utz <michael@theutz.com>
2025-09-19 11:00:06 +0300

    Update .config/nvim/lua/plugins/colorschemes.lua Update .config/nvim/lazy-lock.json

--- dot_config/exact_nvim/exact_lua/exact_plugins/colorschemes.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/colorschemes.lua
@@ -5,4 +5,17 @@ return {
 			colorscheme = "catppuccin",
 		},
 	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		lazy = true,
+		opts = {
+			transparent_background = true,
+			term_colors = true,
+			float = {
+				transparent = true,
+			},
+		},
+	},
 }
