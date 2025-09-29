b61da8a50b03f586ffecfc1d0e6de19e81204d6c
Michael Utz <michael@theutz.com>
2025-09-29 15:38:35 +0300

    Update .config/nvim/lua/config/options.lua Update .config/nvim/lua/plugins/colorschemes.lua Update .config/tmux/tmux.conf

--- dot_config/exact_nvim/exact_lua/exact_config/options.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/options.lua
@@ -18,13 +18,13 @@ vim.o.exrc = true
 -- vim.o.shell = "/opt/homebrew/bin/fish"
 
 -- Use nushell
-vim.o.shell = "/Users/michael/.local/share/mise/shims/nu"
-vim.o.shellcmdflag = "--login --stdin --no-newline -c"
-vim.o.shellredir = "out+err> %s"
-vim.o.shellpipe = "| complete | update stderr { ansi strip } | tee { get stderr | save --force --raw %s } | into record"
-vim.o.shelltemp = false
-vim.o.shellxescape = ""
-vim.o.shellxquote = ""
-vim.o.shellquote = ""
+-- vim.o.shell = "/Users/michael/.local/share/mise/shims/nu"
+-- vim.o.shellcmdflag = "--login --stdin --no-newline -c"
+-- vim.o.shellredir = "out+err> %s"
+-- vim.o.shellpipe = "| complete | update stderr { ansi strip } | tee { get stderr | save --force --raw %s } | into record"
+-- vim.o.shelltemp = false
+-- vim.o.shellxescape = ""
+-- vim.o.shellxquote = ""
+-- vim.o.shellquote = ""
 
 vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
