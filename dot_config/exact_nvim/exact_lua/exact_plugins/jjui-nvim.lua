5641d6210078f072091da437f41c6de528402dd7
Michael Utz <michael@theutz.com>
2025-12-09 12:31:11 +0300

    Add .config/nvim/lua/plugins/jjui-nvim.lua

---
+++ dot_config/exact_nvim/exact_lua/exact_plugins/jjui-nvim.lua
@@ -0,0 +1,25 @@
+return {
+	"HotThoughts/jjui.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+	},
+	cmd = {
+		"JJUI",
+		"JJUICurrentFile",
+		"JJUIFilter",
+		"JJUIFilterCurrentFile",
+		"JJConfig",
+	},
+	-- Setting the keybinding here helps lazy-loading
+	keys = {
+		{ "<leader>jj", "<cmd>JJUI<cr>", desc = "JJUI" },
+		{ "<leader>jc", "<cmd>JJUICurrentFile<cr>", desc = "JJUI (current file)" },
+		{ "<leader>jl", "<cmd>JJUIFilter<cr>", desc = "JJUI Log" },
+		{ "<leader>jf", "<cmd>JJUIFilterCurrentFile<cr>", desc = "JJUI Log (current file)" },
+	},
+	config = function()
+		require("jjui").setup({
+			-- configuration options (see below)
+		})
+	end,
+}
